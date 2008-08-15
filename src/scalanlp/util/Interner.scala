8a373c8ea72e7a15ff1cc4134f785c17395eb0fc
dlwh <dlwh@c9c46bb1-2420-0410-b6d5-8c78f6715463>
2008-08-15 05:51:43 +0000

    Scala interner. Thread-safe
    git-svn-id: svn+ssh://jamie.stanford.edu/u/nlp/svnroot/trunk/scalanlp/projects/core@22088 c9c46bb1-2420-0410-b6d5-8c78f6715463

---
+++ src/scalanlp/util/Interner.scala
@@ -0,0 +1,30 @@
+package scalanlp.util;
+
+import scala.collection.jcl.WeakHashMap;
+import java.lang.ref.WeakReference;
+
+/**
+ * Class that mimics Java's string interner, but for anything.
+ * Threadsafe;
+ */
+class Interner[T] extends (T=>T) {
+  def apply(t :T) = intern(t);
+
+  def intern(t : T) = synchronized {
+    inner.getOrElseUpdate(t,new WeakReference[T](t)).get;
+  }
+
+  def clear() = inner.clear();
+  def size = inner.size;
+
+  def internAll(c : Iterable[T]) = c map apply
+  def internAll(c : Collection[T]) = c map apply
+  def internAll(c : List[T]) = c map apply
+  def internAll(c : Array[T]) = c map apply
+  def internAll(c : Set[T]) = c map apply
+
+  private val inner = new WeakHashMap[T,WeakReference[T]];
+}
+
+object Interner extends Interner[Any] {
+}
