ff7bf826190f65fae87432b4aaaf05730e2b291b
dlwh <dlwh@c9c46bb1-2420-0410-b6d5-8c78f6715463>
2008-09-21 19:04:11 +0000

    StringExtras, with editDistance
    git-svn-id: svn+ssh://jamie.stanford.edu/u/nlp/svnroot/trunk/scalanlp/projects/core@22674 c9c46bb1-2420-0410-b6d5-8c78f6715463

--- src/scalanlp/util/Implicits.scala
+++ src/scalanlp/util/Implicits.scala
@@ -22,4 +22,28 @@ object Implicits {
   }
 
   implicit def listExtras[T](list : List[T]) = new ListExtra(list);
+
+  class StringExtras(s : String) {
+    /**
+    * Implements Levenshtein Distance. 
+    */
+    def editDistance(s2 : String) = {
+      if(s.length == 0) s2.length;
+      else if(s2.length ==0) s.length;
+      else {
+        val d = new Array[Array[Int]](s.length+1,s2.length+1);
+        for(i <- 0 to s.length)
+          d(i)(0) = i;
+        for(i <- 0 to s2.length)
+          d(0)(i) = i;
+        for(i <- 1 to s.length;
+          j <- 1 to s2.length) {
+            val cost = if(s(i) == s2(j)) 0 else 1;
+            d(i)(j) = Math.min( d(i-1)(j)+1, Math.min(d(i)(j-1)+1,d(i-1)(j-1) + cost));
+        }
+        d(s.length)(s2.length);
+      }
+    }
+  }
+  implicit def stringExtras(s : String) = new StringExtras(s);
 }
