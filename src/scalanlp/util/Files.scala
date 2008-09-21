db975f0bcb92467053e6ca83f4de4891dc698729
yeh1 <yeh1@c9c46bb1-2420-0410-b6d5-8c78f6715463>
2008-09-21 01:56:22 +0000

    Added utility wrappers for file i/o
    git-svn-id: svn+ssh://jamie.stanford.edu/u/nlp/svnroot/trunk/scalanlp/projects/core@22664 c9c46bb1-2420-0410-b6d5-8c78f6715463

---
+++ src/scalanlp/util/Files.scala
@@ -0,0 +1,74 @@
+package scalanlp.util
+
+import java.io._;
+
+/**
+ * This contains several convenience routines that wrap file
+ * reading and writing operations with niceities such as 
+ * exception handling.
+ *
+ * @author yeh1
+ */
+object FileUtils {
+
+  /**
+   * Simply opens the target file, opens a BufferedReader on it, and
+   * passes the reader to the operation.  Handles exception wrapping.
+   */
+  def read(file:File)(operation:BufferedReader=>Any):Boolean = {
+    try {
+      var in = new BufferedReader(new FileReader(file));
+      operation(in);
+      true;
+    } catch {
+      case e:Exception => {
+	System.err.println("FileUtils.read: Error "+e.getMessage());
+	e.printStackTrace
+      }
+      false;
+    }
+  }
+
+  /**
+   * Given a target file, and an operation that accepts
+   * a single String line, reads the file in line by line,
+   * applying the given operation until the file is exhausted.
+   */
+  def readByLine(file:File)(operation:String=>Any):Boolean = {
+    try {
+      var in = new BufferedReader(new FileReader(file));
+      var line:String = in.readLine();
+      while (line != null) {
+	operation(line);
+	line = in.readLine();
+      }
+      true;
+    } catch {
+      case e:Exception => {
+	System.err.println("FileUtils.readByLine: Error "+e.getMessage());
+	e.printStackTrace
+      }
+      false;
+    }
+  }
+
+  /**
+   * Given a target file and an operation that accepts a writer, 
+   * simply sets up an exception wrapped writer and calls the operation
+   * with the writer.
+   */
+  def write(file:File)(operation: Writer=>Unit):Boolean  = {
+    try {
+      var out = new BufferedWriter(new FileWriter(file));
+      operation(out);
+      out.close
+      true
+    } catch {
+      case e:Exception => {
+	System.err.println("FileUtils.write: Error "+e.getMessage());
+	e.printStackTrace
+      }
+      false;
+    }
+  }
+}
