782f395c8d2f34e1d5b8fd78ec5a59b57baacaf9
dlwh <dlwh@c9c46bb1-2420-0410-b6d5-8c78f6715463>
2008-11-10 02:10:16 +0000

    Refactoring Dirichlet/Conjugate Prior.
    git-svn-id: svn+ssh://jamie.stanford.edu/u/nlp/svnroot/trunk/scalanlp/projects/core@23268 c9c46bb1-2420-0410-b6d5-8c78f6715463

--- src/scalanlp/stats/ConjugatePrior.scala
+++ src/scalanlp/stats/ConjugatePrior.scala
@@ -3,7 +3,7 @@ package scalanlp.stats;
 /**
  * Trait representing conjugate priors. See Dirichlet for an example.
  */
-trait ConjugatePrior[D,T] extends Distribution[D] {
+trait ConjugatePrior[P,T] extends Distribution[P] {
   /**
    * Returns a distribtution over T's after integrating out the intermediate distributions.
    */
@@ -11,9 +11,9 @@ trait ConjugatePrior[D,T] extends Distribution[D] {
   /**
    * Gives a new ConjugatePrior after observing the evidence. See Dirichlet for an example.
    */
-  def posterior(evidence : Iterable[(T,Double)]) : ConjugatePrior[D,T] = posterior(evidence.elements);
+  def posterior(evidence : Iterable[(T,Double)]) : ConjugatePrior[P,T] = posterior(evidence.elements);
   /**
    * Gives a new ConjugatePrior after observing the evidence. See Dirichlet for an example.
    */
-  def posterior(evidence : Iterator[(T,Double)]) : ConjugatePrior[D,T];
+  def posterior(evidence : Iterator[(T,Double)]) : ConjugatePrior[P,T];
 }
--- src/scalanlp/stats/Dirichlet.scala
+++ src/scalanlp/stats/Dirichlet.scala
@@ -3,13 +3,22 @@ import scalanlp.counters._;
 import scalanlp.counters.DoubleCounter;
 import scalanlp.counters.Counters._;
 import scalanlp.math.Numerics._;
+import scalanlp.math.Arrays._;
 
 /**
  * Represents a Dirichlet distribution, the conjugate prior to the multinomial.
  * @author dlwh
  */
-trait Dirichlet[T] extends ConjugatePrior[Multinomial[T],T] {
+trait Dirichlet[P,T] extends ConjugatePrior[P,T] {
   protected val prior : Iterable[(T,Double)];
+  protected def pFromDraw(it : Iterator[(T,Double)]) : P;
+  protected def componentsFromP(p : P) : Function[T,Double]
+  /**
+   * Returns a new Dirichlet after observing the evidence. 
+   */
+  override def posterior(evidence : Iterator[(T,Double)]): Dirichlet[P,T];
+
+
   private lazy val generators : Iterable[(T,Gamma)] = prior.map { e => (e._1,new Gamma(e._2,1)) }
 
   /**
@@ -21,21 +30,22 @@ trait Dirichlet[T] extends ConjugatePrior[Multinomial[T],T] {
    * Returns a Multinomial distribution over the elements;
    */
   def get() = {
-    Multinomial(aggregate(generators.map(e => (e._1,e._2.get)).elements));
+    pFromDraw(generators.map(e => (e._1,e._2.get)).elements);
   }
 
   /**
    * Returns the log pdf function of the Dirichlet up to a constant evaluated at m
    */
-  override def unnormalizedLogProbabilityOf(m : Multinomial[T]) = {
-    prior.map( e => (e._2-1) * m.logProbabilityOf(e._1)).
+  override def unnormalizedLogProbabilityOf(m : P) = {
+    val f = componentsFromP(m);
+    prior.map( e => (e._2-1) * f(e._1) ).
           foldLeft(0.0)(_+_);
   }
 
   /**
    * Returns the log pdf of the Dirichlet evaluated at m.
    */
-  override def logProbabilityOf(m: Multinomial[T]) = {
+  override def logProbabilityOf(m: P) = {
     unnormalizedLogProbabilityOf(m) + prior.map(e => lgamma(e._2))
       .foldLeft(0.0)(_+_) - lgamma(prior.foldLeft(0.0)(_+_._2));
   }
@@ -43,17 +53,13 @@ trait Dirichlet[T] extends ConjugatePrior[Multinomial[T],T] {
   /**
    * Returns the pdf of the Dirichlet evaluated at m.
    */
-  def probabilityOf(m : Multinomial[T]) = Math.exp(logProbabilityOf(m));
+  def probabilityOf(m : P) = Math.exp(logProbabilityOf(m));
 
   /**
    * Returns a multinomial over T's. Each draw is drawn from E[Dir]
    */
   def predictive : Multinomial[T] = Multinomial(aggregate(prior.elements).normalized);
 
-  /**
-   * Returns a new Dirichlet after observing the evidence. 
-   */
-  override def posterior(evidence : Iterator[(T,Double)]) = Dirichlet(aggregate(prior.elements ++ evidence));
 }
 
 /**
@@ -66,8 +72,13 @@ object Dirichlet {
   /**
    * Creates a new Dirichlet with pseudocounts equal to the observed counts.
    */
-  def apply[T](c : DoubleCounter[T]) = new Dirichlet[T] {
+  def apply[T](c : DoubleCounter[T]): Dirichlet[DoubleCounter[T],T] = new Dirichlet[DoubleCounter[T],T] {
     protected val prior = c.elements.toList; 
+    protected def pFromDraw(it : Iterator[(T,Double)]) = aggregate(it);
+    protected def componentsFromP(p : DoubleCounter[T]) = p;
+    def posterior(it : Iterator[(T,Double)]) : Dirichlet[DoubleCounter[T],T] = {
+      Dirichlet( (DoubleCounter[T]() ++ it ++ prior).asInstanceOf[DoubleCounter[T]]);
+    }
   }
 
   /**
@@ -78,12 +89,30 @@ object Dirichlet {
   /**
    * Creates a new Dirichlet with pseudocounts arr(i)
    */
-  def apply(arr : Array[Double]) = new Dirichlet[Int]{
+  def apply(arr : Array[Double]) : Dirichlet[Array[Double],Int] = new Dirichlet[Array[Double], Int] {
     val prior = arr.zipWithIndex.map{x => (x._2,x._1)};
+    def pFromDraw(it : Iterator[(Int,Double)]) = {
+      val m = Map() ++ it;
+      Array.fromFunction(m)(prior.length);
+    }
+
+    protected def componentsFromP(p : Array[Double]) = {
+      p;
+    }
+
+    def posterior(it : Iterator[(Int,Double)]) = { 
+      val ret = new Array[Double](prior.length);
+      System.arraycopy(arr,0,ret,0,ret.length);
+      for( (t,d) <- it) {
+        ret(t) += d;
+      }
+      Dirichlet(ret);
+    }
+
     // a little faster
     override def get() = {
       val arr = generators.map(e => e._2.get)
-      Multinomial(arr.toSeq.toArray,arr.foldLeft(0.0)(_+_));
+      normalize(arr.toSeq.toArray)
     }
   }
 }
