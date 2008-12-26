fab6407e54860b19c91f0977efa394bb22ff2003
David Hall <dlwh@stanford.edu>
2008-12-25 23:20:40 -0800

    Added stop words processing. Automatically looks in stopwords/language.lst. Messed with JarCorpus

--- src/main/scala/scalanlp/corpora/Corpus.scala
+++ src/main/scala/scalanlp/corpora/Corpus.scala
@@ -21,29 +21,60 @@ trait Corpus[+T] {
 /**
 * Pretty useful for creating a corpus from a jar file where the
 * resources are packaged in the jar file. Simply specify a mapFun
-* to transform the data and a listing of which prefixes we care about.
-*
-* Those prefixes/directories are used as the entries to the maps.
+* to transform the data and and put a "data/categories" file in the 
+* jar that has "splitName resourceName" pairs separated by a space,
+* where the resourceName points to another resource with a list of
+* files to be used in that split. It's easier than it sounds.
 *
 * @author dlwh
 */
 trait JarCorpus[+T] extends Corpus[T] {
-  protected def directories: Array[String];
-  protected def mapFun(path:String, s: scala.io.Source): T
+  protected def mapFun(category:String, path:String, s: scala.io.Source): T
+
+  // Utility to read "k v" pairs from a resource.
+  protected def stringsFromFile(cl: ClassLoader, s : String) = {
+    val strm = cl.getResourceAsStream(s)
+    val src = Source.fromInputStream(strm);
+    val result = (for{ line <- src.getLines;
+                      trimmed = line.trim} 
+                      yield trimmed).collect;
+    strm.close();
+    result;
+  }
+
+  // place for top level categories files
+  protected def categoryFile: String = "data/categories";
+
+  /**
+  * Provides a list of (categoryName,resourceName), where the
+  * categoryName is the name of the split (like "train") and the
+  * resourceName is a resource path that has a list of the files
+  * to load.
+  */
+  protected def categories: Seq[(String,String)] = stringsFromFile(classLoader, categoryFile) map { x=>
+    val res = x.split(" ");
+    (res(0),res(1));
+  };
 
   def splits = {
-    val m = scala.collection.mutable.Map[String,ArrayBuffer[T]]();
-    val jarURL = new URL("jar:"+classLocation).openConnection.asInstanceOf[JarURLConnection]; 
-    val jarFile = jarURL.getJarFile();
-    for(entry <- jarFile.entries;
-        directory <- directories.find(entry.getName startsWith _ )) {
-      val t = mapFun(entry.getName,Source.fromInputStream(jarFile.getInputStream(entry)));
-      m.getOrElseUpdate(directory,new ArrayBuffer[T]) += t;
+    Map[String,Seq[T]]() ++ categories.map { case(cat,res) =>
+      val data = for{ path <- stringsFromFile(classLoader,res);
+                      strm = classLoader.getResourceAsStream(path);
+                      src = Source.fromInputStream(strm)
+                    } yield {
+                      val result = mapFun(cat,path,src);
+                      strm.close();
+                      result;
+                    }
+      (cat,data);
     }
-    Map[String,Seq[T]]() ++ m;
   }
 
   protected lazy val classLocation = {
     this.getClass.getProtectionDomain.getCodeSource.getLocation;
+  }
+
+  protected lazy val classLoader = {
+    this.getClass.getClassLoader;
   }
 }
