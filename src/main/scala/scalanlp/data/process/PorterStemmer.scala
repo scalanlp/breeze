33b7146177475f7267201a4296141c92e79bc8a5
David Hall <dlwh@stanford.edu>
2008-12-31 01:31:15 -0800

    PorterStemmer!

--- src/main/scala/scalanlp/data/process/PorterStemmer.scala
+++ src/main/scala/scalanlp/data/process/PorterStemmer.scala
@@ -5,219 +5,208 @@ object PorterStemmer extends (String=>String){
     if(w.length < 3) w.toLowerCase;
     else {
       val ret = {
-        val w2 = w.toLowerCase;
-        if(w2(0) == 'y') 'Y' + w2.substring(1) else w2;
+        w.toLowerCase.replaceAll("([aeiou])y","$1Y").replaceAll("^y","Y");
       }
-      (step1 andThen step2 andThen step3 andThen step4 andThen step5)(ret);
+      step5(step4(step3(step2(step1(ret))))).toLowerCase;
     }
   }
 
   private def step1(w:String) = step1c(step1b(step1a(w)));
 
+  // get rid of s's
   private def step1a(w:String) = {
-    if(w endsWith "sses" || w endsWith "ies") 
+    if(w.endsWith("sses") || w.endsWith("ies")) 
       w.substring(0,w.length-2);
-    else if(w endsWith 's' && w.charAt(w.length-1) != 's') 
+    else if(w.endsWith("s") && w.charAt(w.length-2) != 's') 
       w.substring(0,w.length-1);
     else w
   }
 
   private def step1b(w:String) = {
+    //println(w + " " + m(w));;
     def extra(w:String) = {
-      if(w endsWith "at" || w endsWith "bl" || w endsWith "iz" ) w + 'e';
+      if(w.endsWith("at") || w.endsWith("bl") || w.endsWith("iz") ) w + 'e';
       // double consonant:
-      else if(w.last == w.charAt(w.length -2) 
-          && isConsonant(w.last) 
-          && ("lsz" indexOf w.last) == -1) w.substring(0,w.length-1); 
-      else if(m(w) == 1 && isCVC(w)) w + "e";
+      else if(doublec(w) && !("lsz".contains(w.last))) w.substring(0,w.length-1); 
+      else if(m(w) == 1 && cvc(w)) w + "e";
       else w;
     }
     
-    if(w endsWith "eed") {
-      if(m(word.substring(0,w.length-3)) > 0)
+    if(w.endsWith("eed")) {
+      if(m(w.substring(0,w.length-3)) > 0)
         w.substring(0,w.length-1);
       else w;
-    } else if(w endsWith "ed") {
-      if(w.findIndexOf(isVowel) < (w.length - 1)) extra(w.substring(w,w.length-2));
+    } else if(w.endsWith("ed")) {
+      if(w.findIndexOf(isVowel) < (w.length - 2)) extra(w.substring(0,w.length-2));
       else w;
-    } else if(w endsWith "ing") {
-      if(w.findIndexOf(isVowel) < (w.length - 1)) extra(w.substring(w,w.length-3));
+    } else if(w.endsWith("ing")) {
+      if(w.findIndexOf(isVowel) < (w.length - 3)) extra(w.substring(0,w.length-3));
       else w;
     } else w;
   }
 
-  private def step1c(w: String) = {
-    if(w endsWith 'y' && w.findIndexOf(isVowel) < w.length -1) {
+  def step1c(w: String) = {
+    //println(w + " " + m(w));;
+    if( (w.last == 'y' || w.last == 'Y') && w.findIndexOf(isVowel) < w.length -1) {
       w.substring(0,w.length-1) + 'i'
     } else w;
   }
 
   private def replaceSuffix(w:String, suffix: String, repl: String) = {
-    if(w endsWith suffix) Right(w.substring(0,w.length - suffix.length) + repl);
-    else Left(w);
+    if(w endsWith suffix) Some( (w.substring(0,w.length - suffix.length),repl));
+    else None
   }
 
-  @inline 
-  private def prefixHasProperty(w:String, suffix: String, repl:String, prop: (String=>Boolean)) = {
-    if(w endsWith suffix) {
-      val prefix =  w.substring(0,w.length - suffix.length);
-      if(prop(prefix)) Right(prefix+ repl) else Left(w)
-    }
-    else Left(w)
-  }
+  private val mgt0 = { (w:(String,String)) => m(w._1) >0}
+  private val mgt1 = { (w:(String,String)) => m(w._1) >1}
 
-  private val mgt0 = { (w:String) => m(w) >0}
-  private val mgt1 = { (w:String) => m(w) >1}
-
-  private def step2(w:String) = {
-    w(w.length-2) match {
-      case 'a' => 
-        val e =
-          for(l <- prefixHasProperty(w,"ational","ate",mgt0).left;
-              l1 <- prefixHasProperty(w,"tional","tion",mgt0).left)
-              yield l1;
-        Either.merge(e);
-      case 'c' =>
-        val e =
-          for(l <- prefixHasProperty(w,"enci","ence",mgt0).left;
-              l1 <- prefixHasProperty(w,"anci","ance",mgt0).left)
-              yield l1;
-        Either.merge(e);
-      case 'e' =>
-        Either.merge(prefixHasProperty(w,"izer","ize",mgt0).left)
-      case 'g' =>
-        Either.merge(prefixHasProperty(w,"logi","log",mgt0).left)
-      case 'l' =>
-        val e =
-          for(l <- prefixHasProperty(w,"bli","ble",mgt0).left;
-              l1 <- prefixHasProperty(w,"alli","al",mgt0).left;
-              l2 <- prefixHasProperty(w,"entli","ent",mgt0).left;
-              l3 <- prefixHasProperty(w,"eli","e",mgt0).left;
-              l4 <- prefixHasProperty(w,"ousli","ous",mgt0).left)
-              yield l4;
-        Either.merge(e);
-      case 'o' =>
-        val e =
-          for(l <- prefixHasProperty(w,"ization","ize",mgt0).left;
-              l1 <- prefixHasProperty(w,"ator","ate",mgt0).left;
-              l2 <- prefixHasProperty(w,"ation","ate",mgt0).left)
-              yield l2;
-        Either.merge(e);
-      case 's' =>
-        val e =
-          for(l <- prefixHasProperty(w,"alism","al",mgt0).left;
-              l1 <- prefixHasProperty(w,"iveness","ive",mgt0).left;
-              l2 <- prefixHasProperty(w,"fulness","ful",mgt0).left;
-              l3 <- prefixHasProperty(w,"ousness","ous",mgt0).left)
-              yield l3;
-        Either.merge(e);
-      case 't' =>
-        val e =
-          for(l <- prefixHasProperty(w,"aliti","al",mgt0).left;
-              l1 <- prefixHasProperty(w,"iviti","ive",mgt0).left;
-              l2 <- prefixHasProperty(w,"biliti","ble",mgt0).left)
-              yield l2;
-        Either.merge(e);
-      case _ => w;
+  private def step2(w:String) = { 
+    //println(w + " " + m(w));;
+    if(w.length < 3) w
+    else {
+      val opt = w(w.length-2) match {
+        case 'a' => replaceSuffix(w,"ational","ate").orElse(replaceSuffix(w,"tional","tion"));
+        case 'c' =>
+          replaceSuffix(w,"enci","ence").orElse(replaceSuffix(w,"anci","ance"));
+        case 'e' => replaceSuffix(w,"izer","ize");
+        case 'g' => replaceSuffix(w,"logi","log");
+        case 'l' => replaceSuffix(w,"bli","ble") orElse {
+          replaceSuffix(w,"alli","al")
+        } orElse {
+          replaceSuffix(w,"entli","ent")
+        } orElse {
+          replaceSuffix(w,"eli","e")
+        } orElse {
+          replaceSuffix(w,"ousli","ous")
+        }
+        case 'o' => replaceSuffix(w,"ization","ize") orElse {
+          replaceSuffix(w,"ator","ate")
+        } orElse {
+          replaceSuffix(w,"ation","ate")
+        }
+        case 's' => replaceSuffix(w,"alism","al") orElse {
+          replaceSuffix(w,"iveness","ive")
+        } orElse {
+          replaceSuffix(w,"fulness","ful")
+        } orElse {
+          replaceSuffix(w,"ousness","ous")
+        }
+        case 't' =>
+        replaceSuffix(w,"aliti","al") orElse {
+          replaceSuffix(w,"iviti","ive")
+        } orElse {
+          replaceSuffix(w,"biliti","ble")
+        }
+        case _ => None;
+      }
+      opt.filter(mgt0).map{ case (a,b) => a+b}.getOrElse(w);
     }
   }
 
   private def step3(w:String) = {
-    w.last match {
-      case 'e' => 
-        val e =
-          for(l <- prefixHasProperty(w,"icate","ic",mgt0).left;
-              l0 <- prefixHasProperty(w,"alize","al",mgt0).left;
-              l1 <- prefixHasProperty(w,"ative","",mgt0).left)
-              yield l1;
-        Either.merge(e);
-      case 'i' => 
-        val e =
-          for(l <- prefixHasProperty(w,"iciti","ic",mgt0).left;
-              yield l;
-        Either.merge(e);
-      case 'l' => 
-        val e =
-          for(l <- prefixHasProperty(w,"ical","ic",mgt0).left;
-              l0 <- prefixHasProperty(w,"ful","",mgt0).left)
-              yield l0;
-        Either.merge(e);
-      case 's' => 
-        val e =
-          for(l <- prefixHasProperty(w,"ness","",mgt0).left)
-              yield l;
-        Either.merge(e);
-      case _ => w;
+    //println(w + " " + m(w));;
+    if(w.length < 3) w else {
+      val opt = w.last match {
+        case 'e' =>
+        replaceSuffix(w,"icate","ic") orElse {
+          replaceSuffix(w,"alize","al")
+        } orElse {
+          replaceSuffix(w,"ative","")
+        }
+        case 'i' => replaceSuffix(w,"iciti","ic");
+        case 'l' => replaceSuffix(w,"ical","ic").orElse(replaceSuffix(w,"ful",""));
+        case 's' => replaceSuffix(w,"ness","");
+        case _ => None;
+      }
+      opt.filter(mgt0).map{ case (a,b) => a+b}.getOrElse(w);
+    }
   }
 
   private def step4(w:String) = {
-    w(w.length-1) match {
-      case 'a' => 
-        val e =
-          for(l <- prefixHasProperty(w,"al","",mgt1).left)
-              yield l;
-        Either.merge(e);
-      case 'c' => 
-        val e =
-          for(l0 <- prefixHasProperty(w,"ance","",mgt1).left;
-              l <- prefixHasProperty(w,"ence","",mgt1).left)
-              yield l;
-        Either.merge(e);
-      case 'e' => 
-        val e =
-          for(l <- prefixHasProperty(w,"er","",mgt1).left)
-              yield l;
-        Either.merge(e);
-      case 'i' => 
-        val e =
-          for(l <- prefixHasProperty(w,"ic","",mgt1).left)
-              yield l;
-        Either.merge(e);
-      case 'l' => 
-        val e =
-          for(l <- prefixHasProperty(w,"able","",mgt1).left;
-              l1 <- prefixHasProperty(w,"ible","",mgt1).left)
-              yield l1;
-        Either.merge(e);
-      case 'n' =>
-        val e =
-          for(l2 <- prefixHasProperty(w,"ant","",mgt1).left;
-              l1 <- prefixHasProperty(w,"ement","",mgt1).left;
-              l3 <- prefixHasProperty(w,"ment","",mgt1).left;
-              l <- prefixHasProperty(w,"ent","",mgt1).left)
-              yield l;
-        Either.merge(e);
-      case 'o' =>
-        val e =
-          for(l2 <- prefixHasProperty(w,"ion","",mgt1).left;
-              l <- prefixHasProperty(w,"ou","",mgt1).left)
-              yield l;
-        Either.merge(e);
-      case 's' =>
-        val e =
-          for(l <- prefixHasProperty(w,"ism","",mgt1).left)
-              yield l;
-        Either.merge(e);
-      case 't' =>
-        val e =
-          for(l2 <- prefixHasProperty(w,"ate","",mgt1).left;
-              l <- prefixHasProperty(w,"iti","",mgt1).left)
-              yield l;
-        Either.merge(e);
-      case 'u' =>
-        val e =
-          for(l <- prefixHasProperty(w,"ous","",mgt1).left)
-              yield l;
-        Either.merge(e);
-      case 'v' =>
-        val e =
-          for(l <- prefixHasProperty(w,"ive","",mgt1).left)
-              yield l;
-        Either.merge(e);
-      
-      
-      
-      
+    //println(w + " " + m(w));;
+    if(w.length < 3)
+      w 
+    else {
+      val opt = w(w.length-2) match {
+        case 'a' => replaceSuffix(w,"al","");
+        case 'c' => replaceSuffix(w,"ance","").orElse(replaceSuffix(w,"ence",""));
+        case 'e' => replaceSuffix(w,"er","");
+        case 'i' => replaceSuffix(w,"ic","");
+        case 'l' => replaceSuffix(w,"able","").orElse(replaceSuffix(w,"ible",""));
+        case 'n' => replaceSuffix(w,"ant","") orElse {
+          replaceSuffix(w,"ement","")
+        } orElse {
+          //println("here");
+          replaceSuffix(w,"ment","")
+        } orElse {
+          //println("hereX");
+          replaceSuffix(w,"ent","")
+        }
+        case 'o' => replaceSuffix(w,"ion","").filter(a => a._1.endsWith("t") || a._1.endsWith("s")).
+                      orElse(replaceSuffix(w,"ou",""));
+        case 's' => replaceSuffix(w,"ism","");
+        case 't' => replaceSuffix(w,"ate","").orElse(replaceSuffix(w,"iti",""));
+        case 'u' => replaceSuffix(w,"ous","");
+        case 'v' => replaceSuffix(w,"ive","");
+        case 'z' => replaceSuffix(w,"ize","");
+        case _ => None;
+      }
+      opt.filter(mgt1).map{ case (a,b) => a+b}.getOrElse(w);
     }
   }
+
+  private def step5(w:String) = {
+    //println(w + " " + m(w));;
+    if(w.length < 3) w else 
+    step5b(step5a(w));
+  }
+
+
+  private def step5a(w: String) = {
+    if(w.length < 3) w else 
+    if(w.last == 'e') {
+      val n = m(w);
+      if(n > 1) w.substring(0,w.length-1)
+      else if(  n == 1 && !cvc(w.substring(0,w.length-1))) w.substring(0,w.length-1)
+      else w;
+    }
+    else {w}
+  }
+
+  private def step5b(w:String) = {
+    //println(w + " " + m(w));;
+    if(w.last == 'l' && doublec(w) && m(w) > 1) w.substring(0,w.length-1)
+    else w;
+  }
+
+  def m(w:String):Int = {
+    val firstV = w.findIndexOf(isVowel);
+    if(firstV == -1) 0;
+    else {
+      var m = 0;
+      var x:Seq[Char] = w.substring(firstV);
+      if(x.isEmpty) m
+      else {
+        while(!x.isEmpty) {
+          x = x.dropWhile(isVowel);
+          if(x.isEmpty) return m;
+          m+=1;
+          if(m>1) return m; // don't need anything bigger than this.
+          x = x.dropWhile(isConsonant);
+        }
+        m;
+      }
+    }
+  }
+
+  private def cvc(w:String) = {
+    w.length > 2 && isConsonant(w.last) && !("wxY" contains w.last) && isVowel(w(w.length-2)) && isConsonant(w.charAt(w.length-3))
+  }
+
+  private def doublec(w:String) = {
+    (w.length > 2 && w.last == w.charAt(w.length -2) && isConsonant(w.last))
+  }
+
+  def isConsonant(letter:Char) = !isVowel(letter);
+  def isVowel(letter:Char) = "aeiouy" contains letter;
 }
