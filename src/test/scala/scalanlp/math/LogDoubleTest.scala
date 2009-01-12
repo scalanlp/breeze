f923700e93c880273236021ae2f457c226b3e8d5
David Hall <dlwh@stanford.edu>
2009-01-11 17:31:20 -0800

    botched merge.

--- src/test/scala/scalanlp/math/LogDoubleTest.scala
+++ src/test/scala/scalanlp/math/LogDoubleTest.scala
@@ -3,7 +3,6 @@ package scalanlp.math;
 import org.scalatest.prop._;
 import org.scalacheck.Arbitrary._
 import org.scalacheck.Prop._
-/*
 object LogDoubleSpecification extends PropSuite with Checkers {
 
   test("add", (d:Double, e:Double)  => { (new LogDouble(d) + new LogDouble(e)).value == d + e})
