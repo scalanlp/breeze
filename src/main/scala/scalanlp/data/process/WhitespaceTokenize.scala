056242e976f2eb02a321e7ae3babb617637c10ab
David Hall <dlwh@stanford.edu>
2009-01-15 20:00:00 -0800

    Data processing updates.

--- src/main/scala/scalanlp/data/process/WhitespaceTokenize.scala
+++ src/main/scala/scalanlp/data/process/WhitespaceTokenize.scala
@@ -1,5 +1,6 @@
 package scalanlp.data.process;
 import scalanlp.util.JavaCollections._;
+import scala.collection.mutable._;
 
 import java.io.BufferedInputStream;
 import scala.io.Source;
@@ -13,6 +14,68 @@ import scala.io.Source;
 object WhitespaceTokenize extends (String=>Iterator[String]) {
   def apply(s:String): Iterator[String] = {
     new java.util.StringTokenizer(s).map(_.asInstanceOf[String]);
+  }
+
+  def apply(src: Source): Iterator[String] = new Iterator[String] {
+    private var nextString : Option[String] = None;
+    def hasNext = {
+      src.hasNext && !nextString.orElse(updateString).isEmpty;
+    }
+
+    private def updateString = {
+      if(!src.hasNext) {
+        nextString = None;
+        nextString;
+      }
+
+      val b = new StringBuilder();
+
+      var stop = false;
+
+      while(src.hasNext && !stop) {
+        val ch = src.next;
+        if(!ch.isWhitespace) {
+          stop = true;
+          b += ch;
+        }
+      }
+
+      stop = false;
+      while(src.hasNext && !stop) {
+        val ch = src.next;
+        if(ch.isWhitespace) {
+          stop = true;
+        } else { 
+          b += ch;
+        }
+      }
+      if(b.isEmpty)
+        nextString = None
+      else nextString = Some(b.toString);
+      nextString
+    }
+
+    def next = {
+      val ret = nextString.orElse(updateString).get
+      nextString = None;
+      ret;
+    }
+
+    
+  }
+}
+
+
+/**
+* Breaks a string into alphabetic components. Uses StringTokenizer
+*
+* @author dlwh
+*/
+object AlphaTokenize extends (String=>Iterator[String]) {
+  def apply(s:String): Iterator[String] = {
+    val ret = (new ArrayBuffer[String])
+    ret ++= s.split("[^A-Za-z]");
+    ret.elements
   }
 
   def apply(src: Source): Iterator[String] = new Iterator[String] {
