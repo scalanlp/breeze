46cfc16fd51941ec639da9a1ca66a83afe6fea46
David Hall <dlwh@stanford.edu>
2009-01-17 12:30:05 -0800

    AlphaTokenize now returns an Array.

--- src/main/scala/scalanlp/data/process/WhitespaceTokenize.scala
+++ src/main/scala/scalanlp/data/process/WhitespaceTokenize.scala
@@ -71,58 +71,8 @@ object WhitespaceTokenize extends (String=>Iterator[String]) {
 *
 * @author dlwh
 */
-object AlphaTokenize extends (String=>Iterator[String]) {
-  def apply(s:String): Iterator[String] = {
-    val ret = (new ArrayBuffer[String])
-    ret ++= s.split("[^A-Za-z]");
-    ret.elements
-  }
-
-  def apply(src: Source): Iterator[String] = new Iterator[String] {
-    private var nextString : Option[String] = None;
-    def hasNext = {
-      src.hasNext && !nextString.orElse(updateString).isEmpty;
-    }
-
-    private def updateString = {
-      if(!src.hasNext) {
-        nextString = None;
-        nextString;
-      }
-
-      val b = new StringBuilder();
-
-      var stop = false;
-
-      while(src.hasNext && !stop) {
-        val ch = src.next;
-        if(!ch.isWhitespace) {
-          stop = true;
-          b += ch;
-        }
-      }
-
-      stop = false;
-      while(src.hasNext && !stop) {
-        val ch = src.next;
-        if(ch.isWhitespace) {
-          stop = true;
-        } else { 
-          b += ch;
-        }
-      }
-      if(b.isEmpty)
-        nextString = None
-      else nextString = Some(b.toString);
-      nextString
-    }
-
-    def next = {
-      val ret = nextString.orElse(updateString).get
-      nextString = None;
-      ret;
-    }
-
-    
+object AlphaTokenize extends (String=>Array[String]) {
+  def apply(s:String) = {
+    s.split("[^A-Za-z]");
   }
 }
