2ddbed9ab4af24a4fd0f7254cdd6d9fa6a429243
David Hall <dlwh@berkeley.edu>
2009-08-27 11:26:52 -0700

    Counters: remove entries that are zero'd out.

--- src/main/scala/scalanlp/counters/DoubleCounter.scala
+++ src/main/scala/scalanlp/counters/DoubleCounter.scala
@@ -55,7 +55,10 @@ trait BaseDoubleCounter[T] extends Tensor1[T] with TrackedStatistics[T] { outer 
   */
   def update(t: T, v: Double) {
     val oldV = counts(t);
-    counts(t) = v;
+    if(v == default)
+      counts -= t;
+    else 
+      counts(t) = v;
     updateStatistics(t,oldV,v);
   }
 
