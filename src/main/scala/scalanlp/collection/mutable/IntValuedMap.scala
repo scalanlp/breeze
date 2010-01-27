e405a4b81d40647e3922fcb82bac8a8d9671c3cb
Daniel Ramage <dramage@cs.stanford.edu>
2010-01-26 22:53:17 -0800

    added FastInt2IntValuedMap

--- src/main/scala/scalanlp/collection/mutable/IntValuedMap.scala
+++ src/main/scala/scalanlp/collection/mutable/IntValuedMap.scala
@@ -17,30 +17,66 @@ package scalanlp.collection.mutable;
 
 import scala.collection.mutable.Map;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
+
+import scala.reflect.Manifest;
+
 trait IntValuedMap[K] extends Map[K,Int] {
   override def apply(k: K): Int;
   override def default(k:K): Int = super.default(k);
 }
 
 object IntValuedMap {
-  def apply[K]():IntValuedMap[K] = new  FastAny2IntMap[K];
-  
+  def apply[K]()(implicit m : Manifest[K]) : IntValuedMap[K] = {
+    m.toString match {
+      case "Int" => new FastInt2IntMap().asInstanceOf[IntValuedMap[K]];
+      case _ => new FastAny2IntMap[K]();
+    }
+  }
+
+  class FastInt2IntMap extends IntValuedMap[Int] {
+    val underlying = new Int2IntOpenHashMap();
+    override def get(k : Int) =
+      if (underlying.containsKey(k)) Some(underlying.get(k)) else None;
+    override def apply(k : Int) =
+      if (underlying.containsKey(k)) underlying.get(k) else 0;
+    override def -=(k : Int) : this.type =
+      { underlying.remove(k); this; }
+    override def +=(k : (Int,Int)) : this.type =
+      { underlying.put(k._1,k._2); this; }
+    override def size =
+      underlying.size;
+    override def update(k : Int, d : Int) =
+      underlying.put(k,d);
+    override def iterator = new Iterator[(Int,Int)] {
+      val outer = underlying.int2IntEntrySet.fastIterator;
+      override def next =
+        { val n = outer.next; (n.getIntKey, n.getIntValue); }
+      override def hasNext =
+        outer.hasNext;
+    }
+  }
+
   class FastAny2IntMap[K] extends IntValuedMap[K] {
     val underlying = new Object2IntOpenHashMap[K];
-    def get(k: K) = if(underlying.containsKey(k)) Some(underlying.getInt(k)) else None;
-    override def apply(k:K) = if(underlying.containsKey(k)) underlying.getInt(k) else default(k);
-    def -=(k:K): this.type = { underlying.remove(k); this; }
-    def +=( k:(K,Int)): this.type = { underlying.put(k._1,k._2); this; }
-    override def size = underlying.size;
-    override def update(k:K, d:Int) = underlying.put(k,d);
-    def iterator = new Iterator[(K,Int)] {
-      val outer = underlying.object2IntEntrySet.iterator;
-      def next = { 
-        val n = outer.next;
-        (n.getKey,n.getIntValue)
-      }
-
-      def hasNext = outer.hasNext;
+    override def get(k: K) =
+      if (underlying.containsKey(k)) Some(underlying.getInt(k)) else None;
+    override def apply(k:K) =
+      if(underlying.containsKey(k)) underlying.getInt(k) else default(k);
+    override def -=(k:K) : this.type =
+      { underlying.remove(k); this; }
+    override def +=( k:(K,Int)) : this.type =
+      { underlying.put(k._1,k._2); this; }
+    override def size =
+      underlying.size;
+    override def update(k:K, d:Int) =
+      underlying.put(k,d);
+    override def iterator = new Iterator[(K,Int)] {
+      val outer = underlying.object2IntEntrySet.fastIterator;
+      override def next =
+        { val n = outer.next; (n.getKey,n.getIntValue) }
+      override def hasNext =
+        outer.hasNext;
     }
   }
 }
