5d69c2d1533a86104c527c71d843eb60fb4f260b
David Hall <dlwh@berkeley.edu>
2010-01-30 22:48:47 -0800

    add in the score.

--- src/main/scala/scalanlp/optimize/CompetitiveLinking.scala
+++ src/main/scala/scalanlp/optimize/CompetitiveLinking.scala
@@ -1,5 +1,4 @@
 package scalanlp.optimize
-package edu.berkeley.nlp.optimize;
 
 import collection.mutable.BitSet;
 
@@ -8,9 +7,9 @@ object CompetitiveLinking {
   /**
    * Performs greedy bipartite matching (aka competitive linking).
    * @params matchingPotentials <n,m> matrix, m >= n
-   * @return matching of size n, with values in 0..m-1
+   * @return matching of size n, with values in 0..m-1, along with the score of the matching
    */
-  def extractMatching(matchingPotentials: Seq[Seq[Double]]):Seq[Int] = {
+  def extractMatching(matchingPotentials: Seq[Seq[Double]]):(Seq[Int],Double) = {
     val n = matchingPotentials.length;
     val m = matchingPotentials(0).length;
     require (m >= n, "Column dimension must be at least the size of row dim.");
@@ -21,14 +20,16 @@ object CompetitiveLinking {
     val leftSet = new BitSet(n);
     val rightSet = new BitSet(m);
     val matching = Array.fill(n)(-1);
+    var score = 0.0;
     for (pred <- preds.iterator.takeWhile(_ => leftSet.size < n || rightSet.size < m);
          if !leftSet(pred.i) && !rightSet(pred.j)) {
       matching(pred.i) = pred.j;
       leftSet.add(pred.i);
       rightSet.add(pred.j);
+      score += pred.v;
     }
     matching foreach { a => assert(a >= 0) };
-    matching;
+    (matching,score);
   }
 
   private case class Prediction(i: Int, j: Int, v: Double);
