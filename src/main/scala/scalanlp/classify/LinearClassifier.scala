69ac8c3ad786584b90e005fbefb59fe4d18be542
David Hall <dlwh@berkeley.edu>
2010-03-27 23:07:55 -0700

    Documentation and licenses

--- src/main/scala/scalanlp/classify/LinearClassifier.scala
+++ src/main/scala/scalanlp/classify/LinearClassifier.scala
@@ -1,4 +1,21 @@
 package scalanlp.classify;
+/*
+ Copyright 2010 David Hall, Daniel Ramage
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+
 
 import scalanlp.util.Index;
 import scalanlp.data._;
@@ -13,6 +30,14 @@ import scalala.Scalala._;
 
 import scalala.tensor.operators.TensorShapes._;
 
+/**
+ * A LinearClassifier is a multi-class classifier with decision
+ * function:
+ * <code>
+ * \hat y_i = \arg\max_y w_y^T x_i + b_y
+ * </code>
+ *
+ */
 @serializable
 @SerialVersionUID(1L)
 class LinearClassifier[L,F,T2<:Tensor2[L,F] with TensorSelfOp[(L,F),T2,Shape2],
