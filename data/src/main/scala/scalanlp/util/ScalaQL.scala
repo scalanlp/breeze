4e58eef04cb7b6334ba9ac4d6918252ca024911c
Daniel Ramage <dramage@cs.stanford.edu>
2010-07-27 16:15:24 -0700

    restored 'groups' function lost in transition to 2.8

--- data/src/main/scala/scalanlp/util/ScalaQL.scala
+++ data/src/main/scala/scalanlp/util/ScalaQL.scala
@@ -17,29 +17,117 @@ package scalanlp.util
 */
 
 
-/** 
-* Database-like operations on iterators 
-* 
+/**
+* Database-like operations on iterators
+*
 * @author(dramage)
 */
 object ScalaQL {
-  
+
   /** Groups the elements of the given sequence into a map by the given key function. */
   def groupBy[E,K](seq : Seq[E], keyF : (E=>K))  : scala.collection.mutable.Map[K,List[E]] =
     groupBy(seq.iterator, keyF, (a:E) => a);
-  
+
   /** Groups the elements of the given sequence into a map by the given key function. */
-  def groupBy[E,K,V](iterator : Iterator[E],
+  def groupBy[E,K,V](elements : Iterator[E],
                      keyF : (E=>K),
                      valF : (E=>V)) : scala.collection.mutable.Map[K,List[V]] = {
     val map = new scala.collection.mutable.HashMap[K,List[V]];
-    for (elem <- iterator) {
+    for (elem <- elements) {
       val key = keyF(elem);
       map(key) = valF(elem) :: map.getOrElse(key, List[V]());
     }
     map;
   }
-  
+
+  class ReadaheadIterator[A](iter : Iterator[A]) extends Iterator[A] {
+    protected val peeked = scala.collection.mutable.ListBuffer[A]();
+
+    override def hasNext = !peeked.isEmpty || iter.hasNext;
+
+    override def next = {
+      if (!peeked.isEmpty) {
+        peeked.remove(0);
+      } else {
+        iter.next;
+      }
+    }
+
+    def peekList(n : Int) = {
+      while (peeked.length < n && iter.hasNext) {
+        peeked.append(iter.next);
+      }
+      peeked.view(0, n);
+    }
+
+    def head = {
+      if (peeked.length < 1) peeked.append(iter.next);
+      peeked.head;
+    }
+  }
+
+  /**
+   * Returns an iterator over groups of consecutive elements in iterA and iterB
+   * that are equal to eachother according to the given comparator.
+   */
+  def groups[A,B](iteratorA : Iterator[A], iteratorB : Iterator[B])(compare : ((A,B) => Int)) : Iterator[(List[A],List[B])] = {
+    val iterA = new ReadaheadIterator(iteratorA);
+    val iterB = new ReadaheadIterator(iteratorB);
+
+    new Iterator[(List[A],List[B])] {
+      override def hasNext = iterA.hasNext || iterB.hasNext;
+      override def next : (List[A],List[B]) = {
+        if (!iterA.hasNext && !iterB.hasNext) {
+          // neither iterator has more elements
+          throw new NoSuchElementException("Both iterators empty");
+        } else if (iterA.hasNext && !iterB.hasNext) {
+          // just iterA
+          return (List(iterA.next),List[B]())
+        } else if (!iterA.hasNext && iterB.hasNext) {
+          // just iterB
+          return (List[A](),List(iterB.next))
+        }
+
+        // take matching elements off top of both iterators
+        var incA = 0
+        var incB = 0
+
+        // increment a while still matching b
+        while (iterA.peekList(incA+1).length == incA+1 && compare(iterA.peekList(incA+1).last, iterB.head) == 0) {
+          incA += 1;
+        }
+
+        // increment b while still matching a
+        while (iterB.peekList(incB+1).length == incB+1 && compare(iterA.head, iterB.peekList(incB+1).last) == 0) {
+          incB += 1;
+        }
+
+        // if have matches, return them
+        if (incA > 0 || incB > 0) {
+          return ((1 to incA).map((x:Int) => iterA.next).toList,
+                  (1 to incB).map((x:Int) => iterB.next).toList)
+        }
+
+        // need to increment one or the other by itself
+        val cc = compare(iterA.head, iterB.head);
+        if (cc < 0) {
+          // increment iterA
+          return (List(iterA.next), List[B]());
+        } else if (cc > 0) {
+          // increment iterB
+          return (List[A](), List(iterB.next));
+        }
+
+        throw new RuntimeException("Unexpected code branch");
+      }
+    }
+  }
+
+  def joinInner[A,B](iterA : Iterator[A], iterB : Iterator[B])(compare : ((A,B) => Int)) : Iterator[(A,B)] = {
+    for (group <- groups(iterA, iterB)(compare); a <- group._1.iterator; b <- group._2.iterator)
+      yield (a,b);
+  }
+
   /**
    * Merges (ordered) iterators by returning the lesser element at the
    * head of each, according to the given comparator.  Ties go to the element
@@ -53,7 +141,7 @@ object ScalaQL {
       /** The merged iterator has more if any head is not None. */
       override def hasNext : Boolean =
         heads.map(_ != None).foldLeft(false)(_||_);
-      
+
       /** Return the smallest element that is currently a list head. */
       override def next : T = {
         val top = heads.zipWithIndex.foldLeft((None.asInstanceOf[Option[T]],-1)) {
@@ -66,14 +154,13 @@ object ScalaQL {
               case x:Any => throw new IllegalStateException(x.toString);
               }
             }
-        
+
         // update the top list and return its value
         heads(top._2) = get(iters(top._2));
         return top._1.get;
       }
-      
+
       def get(iter : Iterator[T]) : Option[T] =
         if (iter.hasNext) Some(iter.next) else None;
   }
-  
 }
