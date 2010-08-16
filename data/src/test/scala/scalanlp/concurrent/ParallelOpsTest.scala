428175a9a2035d389225f791d7b810c83288e543
David Hall <dlwh@berkeley.edu>
2010-08-16 14:06:07 -0700

    added a test for parallel map.

--- data/src/test/scala/scalanlp/concurrent/ParallelOpsTest.scala
+++ data/src/test/scala/scalanlp/concurrent/ParallelOpsTest.scala
@@ -12,5 +12,10 @@ class ParallelOpsTest extends FunSuite with Checkers {
   test("sum works") {
     assert((1 to 100).par.fold(0)(_ + _)(_ + _) === (1 to 100).foldLeft(0)(_ + _));
     assert((1 to 100).par.reduce(_+_) === (1 to 100).foldLeft(0)(_ + _));
+    assert((1 to 100).par.map(2*_).par.reduce(_+_) === 2*(1 to 100).foldLeft(0)(_ + _));
+  }
+
+  test("map preserves order") {
+    assert( (1 to 100).par.map(2*_).toIndexedSeq == (1 to 100).map(2*_).toIndexedSeq)
   }
 }
