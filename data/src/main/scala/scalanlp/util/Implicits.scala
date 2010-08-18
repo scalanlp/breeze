2bcadcd36f2d47ad1c60d09fb7d052788d101bea
David Hall <dlwh@berkeley.edu>
2010-08-18 00:40:02 -0700

    Added a class to hold implicits. So far just Double.closeTo

---
+++ data/src/main/scala/scalanlp/util/Implicits.scala
@@ -0,0 +1,15 @@
+package scalanlp.util
+
+object Implicits extends DoubleImplicits {
+
+}
+
+trait DoubleImplicits {
+  class RichDouble(x: Double) {
+    def closeTo(y: Double, tol: Double=1E-5) = {
+      (math.abs(x - y) / (math.abs(x) + math.abs(y) + 1e-10) < tol);
+    }
+  }
+
+  implicit def scEnrichDouble(x: Double) = new RichDouble(x);
+}
