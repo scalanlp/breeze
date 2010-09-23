6098bfc8affb5704ce9ffac39d85f4db2e0c7c78
David Hall <dlwh@berkeley.edu>
2010-09-22 20:25:03 -0700

    flushed out Graph/Digraph

--- graphs/src/main/scala/scalanlp/graphs/Graph.scala
+++ graphs/src/main/scala/scalanlp/graphs/Graph.scala
@@ -1,7 +1,7 @@
 package scalanlp.graphs
 
 /*
- Copyright 2010 David Hall, Daniel Ramage
+ Copyright 2010 David Hall
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -16,18 +16,24 @@ package scalanlp.graphs
  limitations under the License.
 */
 
+/**
+ * Represents a graph, which consists of Nodes, Edges, and ways to go from Edges to Nodes, and vice-versa.
+ */
 trait Graph {
   type Node;
   type Edge;
 
   def edges: Iterator[Edge];
   def endpoints(e: Edge):(Node,Node);
-  def nodes: Iterator[Node];
+  def nodes: Iterable[Node];
   def edgesTouching(n: Node): Iterator[Edge];
   def neighbors(n: Node): Iterator[Node]
   def getEdge(n1: Node, n2: Node): Option[Edge];
 }
 
+/**
+ * A Digraph refines the notion of a graph, adding sources and sinks to edges.
+ */
 trait Digraph extends Graph {
   def source(e: Edge): Node
   def sink(e: Edge): Node
@@ -35,5 +41,56 @@ trait Digraph extends Graph {
   def edgesFrom(n: Node): Iterator[Edge];
   def edgesTo(n: Node): Iterator[Edge];
   def edgesTouching(n: Node) = edgesFrom(n) ++ edgesTo(n);
+}
 
+object Graph {
+  /**
+   * Constructs a graph from an edge list. The edgelist is the primary data
+   * structure, so it's not recommended.
+   */
+  def fromEdges[N](edges: (N,N)*):Graph{ type Node = N; type Edge = (N,N);} = Digraph.fromEdges(edges:_*);
+  /**
+   * Constructs a graph from an adjacency list. The list must be symmetric.
+   */
+  def fromAdjacencyList[N](adjacencyList: Map[N,Seq[N]]):Graph{ type Node = N; type Edge = (N,N);} = {
+    Digraph.fromAdjacencyList(adjacencyList);
+  }
+
+}
+
+
+object Digraph {
+
+  /**
+   * Constructs a graph from an edge list.
+   */
+  def fromEdges[N](edges: (N,N)*) = {
+    val adjList = edges.groupBy(_._1).mapValues(_.map(_._2));
+    Digraph.fromAdjacencyList(adjList);
+  }
+
+  /**
+   * Constructs a graph from an adjacency list.
+   */
+  def fromAdjacencyList[N](adjacencyList: Map[N,Seq[N]]) = {
+    val reversed: IndexedSeq[(N,N)] = (for( (n,adj) <- adjacencyList.iterator; m <- adj.iterator) yield (n,m)).toIndexedSeq;
+    val groupedReversed = reversed.groupBy(_._1).mapValues(_ map (_._2));
+    new Digraph {
+      type Node = N;
+      type Edge = (N,N);
+
+      def edges = for( (n,adj) <- adjacencyList iterator; m <- adj iterator) yield (n,m);
+      def endpoints(e: Edge):(Node,Node) = e;
+      lazy val nodes = (adjacencyList.keys ++ adjacencyList.values.flatten).toSet
+      def neighbors(n: Node) = adjacencyList.get(n).map(_.iterator).getOrElse(Iterator.empty);
+      def getEdge(n1: Node, n2: Node) = {
+        for(adj <- adjacencyList.get(n1); m <- adj.find(_ == n2)) yield (n1,n2);
+      }
+      def source(e: Edge): Node = e._1
+      def sink(e: Edge): Node = e._2;
+
+      def edgesFrom(n: Node): Iterator[Edge] = adjacencyList.getOrElse(n,Seq.empty).iterator.map(n2 => (n,n2));
+      def edgesTo(n: Node): Iterator[Edge] = groupedReversed.getOrElse(n,Seq.empty).iterator.map(n2 => (n2,n));
+    }
+  }
 }
