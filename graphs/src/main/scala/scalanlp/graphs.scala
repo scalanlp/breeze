0cf15b5705cc326684bb7ee775f6f7798a351d06
David Hall <dlwh@berkeley.edu>
2010-09-24 13:43:01 -0700

    weighted graphs.

--- graphs/src/main/scala/scalanlp/graphs.scala
+++ graphs/src/main/scala/scalanlp/graphs.scala
@@ -5,5 +5,7 @@ package scalanlp;
  * @author dlwh
  */
 package object graphs extends scalanlp.graphs.Search with scalanlp.graphs.Transformations {
-
+  import scalanlp.graphs._;
+  type WeightedGraph[Node,Edge,Weight] = Graph[Node,Edge] with Weighted[Edge,Weight]
+  type WeightedDigraph[Node,Edge,Weight] = Digraph[Node,Edge] with Weighted[Edge,Weight]
 }
