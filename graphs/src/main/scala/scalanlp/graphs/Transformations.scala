8498f6621e9fc08ce9c2259172b4c91568724ae8
David Hall <dlwh@berkeley.edu>
2010-09-29 17:47:31 -0700

    reverse a weighted graph.

--- graphs/src/main/scala/scalanlp/graphs/Transformations.scala
+++ graphs/src/main/scala/scalanlp/graphs/Transformations.scala
@@ -36,6 +36,24 @@ trait Transformations {
     }
   }
 
+
+  def reverseWeighted[Node,Edge,W](g: Weighted[Node,Edge,W] with Digraph[Node,Edge])(implicit reverser: EdgeReverser[Edge]): WeightedDigraph[Node,Edge,W] = {
+    val reversedEdges = g.edges.map(reverser).toIndexedSeq;
+    val groupedBySource = reversedEdges.groupBy(g.source _);
+    new Digraph[Node,Edge]  with Weighted[Node,Edge,W] {
+      def edges = reversedEdges.iterator;
+      def nodes = g.nodes;
+      def endPoints(e: Edge) = g.endpoints(e).swap;
+      def edgesFrom(n: Node) = groupedBySource.getOrElse(n, Seq.empty).iterator;
+      def successors(n: Node) = groupedBySource.getOrElse(n, Seq.empty).map(sink).toSet.iterator;
+      def getEdge(n: Node, n2: Node) = groupedBySource.get(n).flatMap(_.find(e => sink(e) == n2));
+      // edges are already reversed, so use actual source/sink
+      def sink(e: Edge) = g.sink(e);
+      def source(e: Edge) = g.source(e);
+      def weight(e: Edge) = g.weight(e);
+    }
+  }
+
 }
 
 trait EdgeReverser[Edge] extends (Edge=>Edge);
@@ -43,5 +61,11 @@ trait EdgeReverser[Edge] extends (Edge=>Edge);
 object EdgeReverser {
   implicit def revPair[N]= new EdgeReverser[(N,N)] {
     def apply(e: (N,N)) = e.swap;
+  }
+  implicit def revWeightedEdge[N,W]= new EdgeReverser[(N,N,W)] {
+    def apply(e: (N,N,W)) = {
+      val (n,m,w) = e;
+      (m,n,w);
+    }
   }
 }
