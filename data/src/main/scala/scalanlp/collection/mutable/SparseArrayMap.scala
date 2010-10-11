743e6822c772677aee85fcf7ad0c2664ef46a143
David Hall <dlwh@berkeley.edu>
2010-10-11 13:32:07 -0700

    theoretically made SparseArray threadsafe. Now apply no longer autoupdates, but SparseArrayMap does.

--- data/src/main/scala/scalanlp/collection/mutable/SparseArrayMap.scala
+++ data/src/main/scala/scalanlp/collection/mutable/SparseArrayMap.scala
@@ -4,12 +4,18 @@ import scala.collection.generic._;
 import scala.reflect.ClassManifest;
 import scala.collection.mutable._;
 
-class SparseArrayMap[T:ClassManifest]
-(default: =>T, initial:Int=3) extends Map[Int,T] {
+/**
+ * SparseArrayMap is a mutable Int=>T Map backed by a SparseArray.
+ * Unlike most maps, it automatically adds keys when an apply returns the default value.
+ */
+class SparseArrayMap[T:ClassManifest](default: =>T, initial:Int=3) extends Map[Int,T] {
 
   private val arr = new SparseArray[T](Int.MaxValue,default,initial);
 
-  override def apply(i: Int) = arr(i);
+  /**
+   * If the key is not present, SparseArrayMap automatically adds the the default.
+   */
+  override def apply(i: Int) = arr.getOrElseUpdate(i,default);
   override def update(i: Int, t: T) = { arr(i) = t}
   override def get(i: Int) = arr get i;
   override def contains(i: Int) = arr.contains(i);
@@ -17,4 +23,6 @@ class SparseArrayMap[T:ClassManifest]
   override def -=(i: Int) = {arr -= i; this }
 
   override def iterator = arr.iterator;
+
+  def getArray = arr;
 }
