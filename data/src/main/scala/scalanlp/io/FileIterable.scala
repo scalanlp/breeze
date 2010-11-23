82a42262cfc171d23d18f02b0e12d0b58f6aca64
David Hall <dlwh@berkeley.edu>
2010-11-23 12:55:58 -0800

    FileIterable for serializing a bunch of objects to a File

---
+++ data/src/main/scala/scalanlp/io/FileIterable.scala
@@ -0,0 +1,47 @@
+package scalanlp.io
+
+import java.io._
+import java.util.zip.{GZIPOutputStream, GZIPInputStream}
+
+/**
+ * A File iterable is an iterable backed by a file.
+ * @author dlwh
+ */
+class FileIterable[+T](file: File) extends Iterable[T] {
+  def iterator:Iterator[T] = {
+    val in = new ObjectInputStream(new BufferedInputStream(new GZIPInputStream(new FileInputStream(file))));
+    var closed = false;
+    new Iterator[T] {
+      var stash : Option[T] = None;
+      def next = {
+        if(stash == None) consume();
+        val r = stash.get
+        stash = None
+        r
+      }
+
+      def hasNext = stash != None || (!closed && { consume(); stash != None})
+
+      private def consume() = try {
+        stash = Some(in.readObject().asInstanceOf[T]);
+        true;
+      } catch {
+        case e: EOFException =>  in.close(); closed = true; false
+      }
+
+      override protected def finalize() = {
+        if(!closed) in.close();
+      }
+    }
+
+  }
+}
+
+object FileIterable {
+  def write[T](it: Iterable[T], file: File) = {
+    val out = new ObjectOutputStream(new BufferedOutputStream(new GZIPOutputStream(new FileOutputStream(file))));
+    for(t <- it) out.writeObject(t);
+    out.close();
+    new FileIterable[T](file);
+  }
+}
