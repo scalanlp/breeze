f87de9160913bc80a1cd854ad1fd29dd5753dc31
David Hall <dlwh@berkeley.edu>
2011-02-14 17:50:36 -0800

    BatchDiffFunction

--- learn/src/main/scala/scalanlp/optimize/CachedDiffFunction.scala
+++ learn/src/main/scala/scalanlp/optimize/CachedDiffFunction.scala
@@ -27,3 +27,31 @@ class CachedDiffFunction[K,T<:Tensor1[K] with TensorSelfOp[K,T,Shape1Col]](obj: 
     lastGradVal;
   }
 }
+
+/**
+ *
+ * @author dlwh
+ */
+class CachedBatchDiffFunction[K,T<:Tensor1[K] with TensorSelfOp[K,T,Shape1Col]](obj: BatchDiffFunction[K,T]) extends BatchDiffFunction[K,T] {
+  /** calculates the gradient at a point */
+  override def gradientAt(x: T, range: IndexedSeq[Int]): T = calculate(x,range)._2;
+  /** calculates the value at a point */
+  override def valueAt(x:T, range: IndexedSeq[Int]): Double = calculate(x,range)._1;
+
+  private var lastX : T = _;
+  private var lastRange : IndexedSeq[Int] = _;
+  private var lastGradVal : (Double,T) = _;
+
+  def fullRange = obj.fullRange;
+
+  /** Calculates both the value and the gradient at a point */
+  override def calculate(x:T, range: IndexedSeq[Int]):(Double,T) = {
+    if(x != lastX || range != lastRange) {
+      lastGradVal = obj.calculate(x, range);
+      lastX = x.copy;
+      lastRange = range;
+    }
+
+    lastGradVal;
+  }
+}
