9a2d7ac30250ebcebb8484aed5025fcc7008d21a
David Hall <dlwh@berkeley.edu>
2011-04-01 01:32:20 -0700

    PairIndex is an index over pairs.

---
+++ data/src/main/scala/scalanlp/util/PairIndex.scala
@@ -0,0 +1,22 @@
+package scalanlp.util
+
+/**
+ * 
+ * @author dlwh
+ */
+
+class PairIndex[T,U](tIndex: Index[T], uIndex: Index[U]) extends Index[(T,U)] {
+  def iterator = for( t <- tIndex iterator; u <- uIndex iterator) yield (t,u);
+
+  def pairs = iterator.zipWithIndex;
+
+  def unapply(i: Int) = if(i >= 0) {
+    val ti =  i / uIndex.size;
+    val ui = i % uIndex.size;
+    for(t <- tIndex.unapply(ti); u <- uIndex.unapply(ui)) yield t -> u;
+  } else {
+    None
+  }
+
+  def apply(t: (T, U)) = tIndex(t._1) * uIndex.size + uIndex(t._2);
+}
