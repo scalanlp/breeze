3cca2ec5a0887e5153af170cbfc8cad8332077c8
David Hall <dlwh@berkeley.edu>
2011-05-09 12:13:34 -0700

    no more proguard

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,7 +2,7 @@ import sbt._
 import java.util.jar.Attributes.Name._
 
 
-class Project(info: ProjectInfo) extends DefaultProject(info) with ProguardProject {
+class Project(info: ProjectInfo) extends DefaultProject(info) {
   // 
   // repositories
   //
