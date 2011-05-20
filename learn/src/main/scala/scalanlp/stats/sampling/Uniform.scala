500cb40cbb5b5f4b205f8f620aedbab040f57d20
David Hall <dlwh@berkeley.edu>
2011-05-20 01:00:02 -0700

    uniform distribution

---
+++ learn/src/main/scala/scalanlp/stats/sampling/Uniform.scala
@@ -0,0 +1,22 @@
+package scalanlp.stats.sampling
+
+/**
+ * 
+ * @author dlwh
+ */
+
+case class Uniform(low: Double, high: Double)(implicit rand: RandBasis = Rand) extends ContinuousDistr[Double] with Moments[Double] {
+  require(low <= high, "low <= high")
+  def draw() = rand.uniform.get * (high - low) + low;
+
+
+  def unnormalizedLogPdf(x: Double) = 0.0
+
+  def logNormalizer = entropy;
+
+  def mode = mean;
+
+  def mean = (low + high)/2
+  def variance = math.pow(high - low,2)/12
+  def entropy = math.log(high - low)
+}
---
+++ learn/src/test/scala/scalanlp/stats/sampling/UniformTest.scala
@@ -0,0 +1,43 @@
+package scalanlp.stats.sampling
+
+/*
+ Copyright 2009 David Hall, Daniel Ramage
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at 
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License. 
+*/
+
+import org.scalatest._;
+import org.scalatest.junit._;
+import org.scalatest.prop._;
+import org.scalacheck._;
+import org.junit.runner.RunWith
+
+
+
+@RunWith(classOf[JUnitRunner])
+class UniformTest extends FunSuite with Checkers with MomentsTestBase[Double] {
+
+  import Arbitrary.arbitrary;
+
+  def asDouble(x: Double) = x
+
+
+  def fromDouble(x: Double) = x
+
+  implicit def arbDistr = Arbitrary {
+    for(a <- arbitrary[Double].map{_.abs % 10000.0};
+        b <- arbitrary[Double].map {_.abs % 10000.0}) yield new Uniform(a min b,b min a);
+  }
+
+
+}  
