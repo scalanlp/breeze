4fab54b31aa15b6ff7c7f3742baf9ecbfb6b1389
David Hall <dlwh@berkeley.edu>
2011-05-25 00:20:14 -0700

    Seqs viewable as Map[Int,T]

--- data/src/main/scala/scalanlp/util/SeqImplicits.scala
+++ data/src/main/scala/scalanlp/util/SeqImplicits.scala
@@ -13,6 +13,20 @@ trait SeqImplicits {
     def argmin(implicit ordering: Ordering[T]) = {
       s.zipWithIndex.reduceLeft( (a,b) => if(ordering.lt(a._1,b._1)) a else b)._2;
     }
+
+    def asMap = new Map[Int,T] {
+      def -(key: Int) = Map() ++ this - key;
+
+      def +[B1 >: T](kv: (Int, B1)) = Map() ++ this + kv
+
+      def get(key: Int) = {
+          if(key >= 0 && key < s.length) Some(s(key))
+          else None
+        }
+
+      def iterator = 0 until s.length zip s iterator;
+
+    }
   }
 
   implicit def seqExtras[T](s: Seq[T]) = new SeqExtras(s);
