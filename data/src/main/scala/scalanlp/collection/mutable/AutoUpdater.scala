76f080c6741376ae9cda8c599acadc633696a10a
David Hall <dlwh@berkeley.edu>
2011-05-25 00:18:58 -0700

    AutoUpdater makes apply calls to a map insert hte default into the map
    fixes to autoupdater? REBASE

---
+++ data/src/main/scala/scalanlp/collection/mutable/AutoUpdater.scala
@@ -0,0 +1,26 @@
+package scalanlp.collection.mutable
+
+import scala.collection.mutable._
+
+/**
+ * AutoUpdater wraps a Map such that any call to apply updates the map with an instance of the default value
+ * @author dlwh
+ */
+class AutoUpdater[M,K,V](val theMap: M, default: =>V)(implicit ev: M<:<Map[K,V]) extends Map[K,V] {
+  override def apply(k: K) = theMap.getOrElseUpdate(k,default);
+  override def update(k: K, v: V) = theMap.update(k,v);
+
+  def +=(kv: (K, V)):this.type = {theMap += kv; this};
+
+  def get(key: K) = theMap.get(key);
+
+  def iterator = theMap.iterator
+
+  def -=(key: K):this.type = { theMap -= key; this}
+
+  override def size = theMap.size;
+}
+
+object AutoUpdater {
+  def apply[M, K, V](map: M, default: =>V)(implicit ev: M<:<Map[K,V]) = new AutoUpdater[M,K,V](map, default);
+}
