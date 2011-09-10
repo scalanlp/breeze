c429c6cc3298c6d66e2fde96e726b1a368643dfa
David Hall <dlwh@berkeley.edu>
2011-09-09 23:21:39 -0700

    SparseFeatureDataset, for reading in ML-Comp style datasets

---
+++ data/src/main/scala/scalanlp/data/SparseFeatureDataset.scala
@@ -0,0 +1,63 @@
+package scalanlp.data
+
+import scalala.tensor.sparse.SparseVector
+import io.Source
+import collection.mutable.ArrayBuffer
+import scalala.collection.sparse.SparseArray
+import scalanlp.serialization.TextSerialization.Readable
+import scalanlp.serialization.{SerializationFormat, TextSerialization}
+import scalanlp.io.TextReader
+
+/**
+ * Dataset of the form <output> [featureIndex:featureValue*]
+ * @author dlwh
+ */
+trait SparseFeatureDataset[Output] {
+  def examples: Seq[Example[Output,SparseVector[Double]]]
+}
+
+object SparseFeatureDataset {
+  def fromSource[Output:TextSerialization.Readable](s: Source, name: String=""):SparseFeatureDataset[Output] = {
+    val impl = implicitly[TextSerialization.Readable[Output]]
+
+    var maxIndex = 0
+
+    implicit val dataReadable = new Readable[(Output,ArrayBuffer[Int],ArrayBuffer[Double])] {
+      def read(source: TextSerialization.Input):(Output,ArrayBuffer[Int],ArrayBuffer[Double]) = {
+        val output = impl.read(source)
+        val line = source.readLine()
+
+        val indices = new ArrayBuffer[Int]
+        val values = new ArrayBuffer[Double]
+        var offset = 0
+        while(offset < line.length) {
+          val nextColon = line.indexOf(':',offset)
+          indices += line.substring(offset,nextColon).trim().toInt
+
+          var nextSpace = line.indexOf(' ', nextColon)
+          if(nextSpace < 0) nextSpace = line.length
+          values += line.substring(nextColon+1,nextSpace).toDouble
+          offset = nextSpace + 1
+        }
+
+        maxIndex = maxIndex max indices.max
+        (output,indices,values)
+
+      }
+    }
+
+    val data = {for(line <- s.getLines()) yield dataReadable.read(TextReader.fromString(line))}.toIndexedSeq
+
+    val processed = for( ((output,indices,values),id) <- data.zipWithIndex) yield {
+      println(output,indices,values,id,maxIndex)
+      val sparseVector = SparseVector.create(maxIndex+1)((indices zip values):_*)
+      Example(output, sparseVector, name + "-" + id)
+    }
+
+    new SparseFeatureDataset[Output] {
+      def examples = processed
+    }
+
+
+  }
+}
---
+++ data/src/test/scala/scalanlp/data/SparseFeatureDatasetTest.scala
@@ -0,0 +1,25 @@
+package scalanlp.data
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import scalala.tensor.sparse.SparseVector
+
+/**
+ * 
+ * @author dlwh
+ */
+
+@RunWith(classOf[JUnitRunner])
+class SparseFeatureDatasetTest extends FunSuite {
+  test("Basic MLComp example") {
+    val result = SparseFeatureDataset.fromSource[Int](scala.io.Source.fromString("""-1 1:1 3:1
+1 2:1 3:1
+-1 1:1 3:1
+-1 1:1"""))
+    assert(result.examples(0).label === -1)
+    assert(result.examples(0).features === SparseVector(0.,1.,0.,1.))
+    assert(result.examples(3).features === SparseVector(0.,1.,0.,0.))
+  }
+
+}
