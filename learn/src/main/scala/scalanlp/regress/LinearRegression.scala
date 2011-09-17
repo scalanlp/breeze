87050992b163f871d7b042a6dc0dbed1d353f2ed
David Hall <dlwh@berkeley.edu>
2011-09-17 13:36:55 -0700

    a simple little linear regression model

---
+++ learn/src/main/scala/scalanlp/regress/LinearRegression.scala
@@ -0,0 +1,20 @@
+package scalanlp.regress
+
+import scalala.tensor.dense.{DenseVectorCol, DenseVector, DenseMatrix}
+
+/**
+ * 
+ * @author dlwh
+ */
+object LinearRegression {
+  /**
+   * Given a set of data points as rows in a matrix and their corresponding outputs, produces a vector of weights
+   * s.t. output(i) \approx observations(i) dot weights
+   */
+  def regress(observations: DenseMatrix[Double], outputs: DenseVectorCol[Double]):DenseVector[Double] = {
+    val cov =  (DenseMatrix.zeros[Double](observations.numCols,observations.numCols) + (observations.t * observations))
+    val scaled = DenseVector.zeros[Double](observations.numCols) + (observations.t * outputs)
+    cov \ scaled
+  }
+
+}
