574800989ad768fc79e585ff583809a3d70d46b9
David Hall <dlwh@berkeley.edu>
2012-01-17 14:03:58 -0800

    made Perceptron yield LinearClassifier

--- learn/src/main/scala/scalanlp/classify/Perceptron.scala
+++ learn/src/main/scala/scalanlp/classify/Perceptron.scala
@@ -2,42 +2,27 @@ package scalanlp.classify
 
 import scalanlp.data.Example
 import scalala.tensor.Counter
-import scalala.generic.collection.CanCreateZerosLike
 import scalala.operators._
-
-/**
- *
- * @author dlwh
- */
-trait Perceptron[L,-T] extends Classifier[L,T] with Serializable;
-
+import bundles.MutableInnerProductSpace
+import scalala.generic.math.CanNorm
 
 object Perceptron {
-  class Trainer[L,T](maxPasses: Int = 20)(implicit dotProduct: BinaryOp[T,T,OpMulInner,Double],
-                       zeros: CanCreateZerosLike[T,T],
-                       numeric: T=>MutableNumericOps[T],
-                       upAdd: BinaryUpdateOp[T,T,OpAdd],
-                       upSub: BinaryUpdateOp[T,T,OpSub]) extends Classifier.Trainer[L,T] {
-    type MyClassifier = Perceptron[L,T];
-    def train(it: Iterable[Example[L,T]]) = {
-      val weights = new collection.mutable.HashMap[L,T]();
-      val result = new Perceptron[L,T] {
-        def scores(o: T) = {
-          val r = Counter[L,Double];
-          for((l,w) <- weights) {
-            r(l) = w dot o;
-          }
-          r
-        }
-      };
-      for( i <- 0 until maxPasses;  ex <- it) {
+  class Trainer[L,T](maxPasses: Int = 20)(implicit vspace: MutableInnerProductSpace[Double,T], canNorm: CanNorm[T]) extends Classifier.Trainer[L,T] {
+    import vspace._
+    type MyClassifier = LinearClassifier[L,LFMatrix[L,T],Counter[L,Double],T];
+    def train(data: Iterable[Example[L,T]]) = {
+      val weights = new LFMatrix[L,T](zeros(data.head.features));
+      weights(data.head.label); // seed with one label
+      import LFMatrix._
+      val result:MyClassifier = new LinearClassifier(weights,Counter[L,Double]());
+      for( i <- 0 until maxPasses;  ex <- data) {
         val l = ex.label;
         val feats = ex.features;
         val ctr = result.scores(feats);
         if(ctr.size == 0 || ctr.argmax != l) {
-          weights.getOrElseUpdate(l,zeros(feats)) += feats;
+          weights(l) += feats;
           if(ctr.size != 0) {
-            weights.getOrElseUpdate(ctr.argmax,zeros(feats)) -= feats;
+            weights(ctr.argmax) -= feats;
           }
         }
       }
