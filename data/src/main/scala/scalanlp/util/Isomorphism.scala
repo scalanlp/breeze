20c1915a377b86f77a5a977d8057870999ff7624
David Hall <dlwh@berkeley.edu>
2012-01-24 22:04:39 -0800

    A better Lens.

--- data/src/main/scala/scalanlp/util/Isomorphism.scala
+++ data/src/main/scala/scalanlp/util/Isomorphism.scala
@@ -3,7 +3,7 @@ package scalanlp.util
 import scalala.tensor.dense.DenseVector
 
 /**
- * A Lens is defined by an isomorphism between two types. useful
+ * An Isomorphism is defined by a reversible transformation between two types. useful
  * when one set of implicits is easily defined for some type, but not for some other type
  * @author dlwh
  */
@@ -23,13 +23,19 @@ object Isomorphism {
     def backward(t: U) = ut(t);
   }
 
+  implicit def identity[T]: Isomorphism[T,T] = new Isomorphism[T,T] {
+    def forward(t: T) = t
+
+    def backward(u: T) = u
+  }
+
   // Scalala lenses:
   implicit object doubleIsVector extends Isomorphism[Double,DenseVector[Double]] {
     def forward(t: Double) = DenseVector(t)
     def backward(t: DenseVector[Double]) = { assert(t.size == 1); t(0)}
   }
 
-   implicit object pdoubleIsVector extends Isomorphism[(Double,Double),DenseVector[Double]] {
+  implicit object pdoubleIsVector extends Isomorphism[(Double,Double),DenseVector[Double]] {
     def forward(t: (Double,Double)) = DenseVector(t._1,t._2)
     def backward(t: DenseVector[Double]) = { assert(t.size == 2); (t(0),t(1))}
   }
