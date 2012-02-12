365e7e6f5881ad15b7e6df9f2be3bdaaaa4e3a1c
David Hall <dlwh@berkeley.edu>
2012-02-12 12:05:31 -0600

    docs for adaptive gradient descent.

--- learn/src/main/scala/scalanlp/optimize/AdaptiveGradientDescent.scala
+++ learn/src/main/scala/scalanlp/optimize/AdaptiveGradientDescent.scala
@@ -7,13 +7,27 @@ import scalala.tensor._
 import scalala.library.Library._
 import scalala.generic.math.CanNorm
 import scalala.generic.collection.{CanMapKeyValuePairs, CanCreateZerosLike, CanMapValues, CanViewAsTensor1}
-;
 
 /**
+ * Implements the L2^2 and L1 updates from
+ * Duchi et al 2010 Adaptive Subgradient Methods for Online Learning and Stochastic Optimization.
+ *
+ * Basically, we use "forward regularization" and an adaptive step size based
+ * on the previous gradients.
  *
  * @author dlwh
  */
 object AdaptiveGradientDescent {
+
+  /**
+   * Implements the L2 regularization update.
+   *
+   * Each step is:
+   *
+   * x_{t+1}i = (s_{ti} * x_{ti} - \eta * g_ti) / (eta * regularization + delta + s_ti)
+   *
+   * where g_ti is the gradient and s_ti = \sqrt(\sum_t'^{t} g_ti^2)
+   */
   trait L2Regularization[T] extends StochasticGradientDescent[T] {
 
     val lambda: Double = 1.0;
@@ -48,6 +62,16 @@ object AdaptiveGradientDescent {
 
   }
 
+
+  /**
+   * Implements the L1 regularization update.
+   *
+   * Each step is:
+   *
+   * x_{t+1}i = sign(x_{t,i} - eta/s_i * g_ti) * (abs(x_ti - eta/s_ti * g_ti) - lambda * eta /s_ti))_+
+   *
+   * where g_ti is the gradient and s_ti = \sqrt(\sum_t'^{t} g_ti^2)
+   */
   class L1Regularization[K,T](val lambda: Double=1.0,
                               delta: Double = 1E-5,
                               eta: Double=4,
