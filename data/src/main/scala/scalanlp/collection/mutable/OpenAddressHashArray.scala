67fcf44838ce72a54dc1dfb139bd980ba9cc996e
David Hall <dlwh@berkeley.edu>
2012-03-10 22:17:46 -0800

    speedups to OAH.

--- data/src/main/scala/scalanlp/collection/mutable/OpenAddressHashArray.scala
+++ data/src/main/scala/scalanlp/collection/mutable/OpenAddressHashArray.scala
@@ -4,37 +4,6 @@ import collection.mutable.BitSet
 import scalala.collection.sparse.DefaultArrayValue
 import actors.threadpool.Arrays
 
-trait ConfigurableDefault[V] extends Serializable {
-  def value(implicit default: DefaultArrayValue[V]):V
-  def fillArray(arr: Array[V], v: V) = arr.asInstanceOf[AnyRef] match {
-    case x: Array[Int] => Arrays.fill(arr.asInstanceOf[Array[Int]], v.asInstanceOf[Int])
-    case x: Array[Long] => Arrays.fill(arr.asInstanceOf[Array[Long]], v.asInstanceOf[Long])
-    case x: Array[Short] => Arrays.fill(arr.asInstanceOf[Array[Short]], v.asInstanceOf[Short])
-    case x: Array[Double] => Arrays.fill(arr.asInstanceOf[Array[Double]], v.asInstanceOf[Double])
-    case x: Array[Float] => Arrays.fill(arr.asInstanceOf[Array[Float]], v.asInstanceOf[Float])
-    case x: Array[Char] => Arrays.fill(arr.asInstanceOf[Array[Char]], v.asInstanceOf[Char])
-    case x: Array[Byte] => Arrays.fill(arr.asInstanceOf[Array[Byte]], v.asInstanceOf[Byte])
-    case x: Array[_] => Arrays.fill(arr.asInstanceOf[Array[AnyRef]], v.asInstanceOf[AnyRef])
-    case _ => throw new RuntimeException("shouldn't be here!")
-  }
-}
-
-trait LowPriorityConfigurableImplicits {
-  implicit def default[V]: ConfigurableDefault[V] = {
-    new ConfigurableDefault[V] {
-      def value(implicit default: DefaultArrayValue[V]) = default.value
-    }
-  }
-
-}
-
-object ConfigurableDefault extends LowPriorityConfigurableImplicits {
-  implicit def fromV[V](v: V):ConfigurableDefault[V] = {
-    new ConfigurableDefault[V] {
-      def value(implicit default: DefaultArrayValue[V]) = v
-    }
-  }
-}
 
 
 /**
@@ -43,15 +12,25 @@ object ConfigurableDefault extends LowPriorityConfigurableImplicits {
  * @author dlwh
  */
 @SerialVersionUID(1)
-class OpenAddressHashArray[@specialized V:ClassManifest:DefaultArrayValue](val size: Int,
-                                                                           default: ConfigurableDefault[V] = ConfigurableDefault.default[V],
-                                                                           initialSize:Int = 16) extends ArrayLike[V] with Serializable {
-  private var index : Array[Int] = new Array[Int](math.max(initialSize,1))
-  private var values: Array[V] = new Array[V](math.max(initialSize,1))
-  default.fillArray(values, default.value)
-  private var occupied = new BitSet()
-  private var load = 0;
+class OpenAddressHashArray[@specialized V] private (val size: Int,
+                                                    private var index : Array[Int],
+                                                    private var values: Array[V],
+                                                    default: ConfigurableDefault[V],
+                                                    private var occupied:BitSet = new BitSet(),
+                                                    private var load: Int = 0)
+                                                   (implicit man: ClassManifest[V],
+                                                    dav: DefaultArrayValue[V]) extends ArrayLike[V] with Serializable {
 
+  def this(size: Int,
+           default: ConfigurableDefault[V] = ConfigurableDefault.default[V],
+           initialSize:Int = 16)(
+    implicit man: ClassManifest[V],
+    dav: DefaultArrayValue[V]) = {
+    this(size,
+         new Array[Int](math.max(initialSize,1)),
+         default.makeArray(math.max(initialSize,1))(dav,man),
+         default)
+  }
 
   def keysIterator = occupied.iterator.map(index)
   def length = size
@@ -63,13 +42,13 @@ class OpenAddressHashArray[@specialized V:ClassManifest:DefaultArrayValue](val s
 
   def update(i: Int, v: V) {
     val pos = locate(i)
-    if(!occupied(pos) && load * 4 >= index.size * 3) {
+    if(!occupied.contains(pos) && load * 4 >= index.size * 3) {
       rehash()
       update(i,v)
     } else {
       index(pos) = i
       values(pos) = v
-      if(!occupied(pos)) {
+      if(!occupied.contains(pos)) {
         occupied += pos
         load += 1
         if(load * 4 >= index.size * 3) rehash()
@@ -85,13 +64,10 @@ class OpenAddressHashArray[@specialized V:ClassManifest:DefaultArrayValue](val s
     if(i >= size) throw new IndexOutOfBoundsException(i + " greater than size of " + size)
     if(i < 0) throw new IndexOutOfBoundsException(i + " less than 0")
     var hash = i.## % index.length
-    val start = hash
-    var ok = true
-    while(occupied(hash) && index(hash) != i) {
+    val len = index.length
+    while(index(hash) != i && occupied.contains(hash)) {
       hash += 1
-      hash %= index.length
-      ok = (hash != start)
-      if(!ok) throw new Exception("We ended up with a full hash table somehow. Bad!")
+      hash %= len
     }
     hash
   }
@@ -112,5 +88,59 @@ class OpenAddressHashArray[@specialized V:ClassManifest:DefaultArrayValue](val s
 
   override def toString = {for( o <- occupied iterator) yield (index(o),values(o))}.mkString("OpenAddressHashArray(",",",")");
 
+  def map[U:DefaultArrayValue:ClassManifest](f: V=>U) = {
+    new OpenAddressHashArray[U](size,
+      Arrays.copyOf(index, index.size),
+      values map f,
+      default map f,
+      BitSet()++occupied,
+      load)
+  }
 
+
+}
+
+
+
+trait ConfigurableDefault[V] extends Serializable { outer =>
+  def value(implicit default: DefaultArrayValue[V]):V
+
+  def fillArray(arr: Array[V], v: V) = arr.asInstanceOf[AnyRef] match {
+    case x: Array[Int] => Arrays.fill(arr.asInstanceOf[Array[Int]], v.asInstanceOf[Int])
+    case x: Array[Long] => Arrays.fill(arr.asInstanceOf[Array[Long]], v.asInstanceOf[Long])
+    case x: Array[Short] => Arrays.fill(arr.asInstanceOf[Array[Short]], v.asInstanceOf[Short])
+    case x: Array[Double] => Arrays.fill(arr.asInstanceOf[Array[Double]], v.asInstanceOf[Double])
+    case x: Array[Float] => Arrays.fill(arr.asInstanceOf[Array[Float]], v.asInstanceOf[Float])
+    case x: Array[Char] => Arrays.fill(arr.asInstanceOf[Array[Char]], v.asInstanceOf[Char])
+    case x: Array[Byte] => Arrays.fill(arr.asInstanceOf[Array[Byte]], v.asInstanceOf[Byte])
+    case x: Array[_] => Arrays.fill(arr.asInstanceOf[Array[AnyRef]], v.asInstanceOf[AnyRef])
+    case _ => throw new RuntimeException("shouldn't be here!")
+  }
+
+  def makeArray(size:Int)(implicit default: DefaultArrayValue[V], man: ClassManifest[V]) = {
+    val arr = new Array[V](size)
+    fillArray(arr,value(default))
+    arr
+  }
+
+  def map[U](f: V=>U)(implicit dav: DefaultArrayValue[V]) = new ConfigurableDefault[U] {
+    def value(implicit default: DefaultArrayValue[U]) = f(outer.value(dav))
+  }
+}
+
+trait LowPriorityConfigurableImplicits {
+  implicit def default[V]: ConfigurableDefault[V] = {
+    new ConfigurableDefault[V] {
+      def value(implicit default: DefaultArrayValue[V]) = default.value
+    }
+  }
+
+}
+
+object ConfigurableDefault extends LowPriorityConfigurableImplicits {
+  implicit def fromV[V](v: V):ConfigurableDefault[V] = {
+    new ConfigurableDefault[V] {
+      def value(implicit default: DefaultArrayValue[V]) = v
+    }
+  }
 }
