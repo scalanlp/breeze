7d299aab72136120ab269316c7bf59f25d05c59d
David Hall <dlwh@berkeley.edu>
2012-03-15 10:51:07 -0700

    don't adjust regularization. expect users to do that themselves.

--- learn/src/main/scala/scalanlp/optimize/FirstOrderMinimizer.scala
+++ learn/src/main/scala/scalanlp/optimize/FirstOrderMinimizer.scala
@@ -87,8 +87,7 @@ object FirstOrderMinimizer {
                        TKVPairs: CanMapKeyValuePairs[T,K,Double,Double,T],
                        view:  <:<[T,scalala.tensor.mutable.Tensor1[K,Double] with scalala.tensor.mutable.TensorLike[K, Double, _, T with scalala.tensor.mutable.Tensor1[K,Double]]]): Iterator[FirstOrderMinimizer[T, BatchDiffFunction[T]]#State] = {
       val it = if(useStochastic) {
-        val adjustedRegularization = regularization * 0.01 * batchSize / f.fullRange.size
-         this.copy(regularization=adjustedRegularization).iterations(f.withRandomBatches(batchSize), init)
+         this.iterations(f.withRandomBatches(batchSize), init)
       } else {
         iterations(f:DiffFunction[T], init)
       }
