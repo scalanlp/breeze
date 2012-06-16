d034ac59784faff41a0057206a5a4403ee9e52a4
David Hall <dlwh@berkeley.edu>
2012-06-15 22:28:21 -0700

    linspace

--- math/src/main/scala/breeze/linalg/package.scala
+++ math/src/main/scala/breeze/linalg/package.scala
@@ -17,5 +17,14 @@ package object linalg {
     new DenseVector(m.data, m.offset, m.majorStride + 1, m.rows)
   }
 
+  /**
+   * Generates a vector of linearly spaced values between a and b (inclusive).
+   * The returned vector will have length elements, defaulting to 100.
+   */
+  def linspace(a : Double, b : Double, length : Int = 100) : DenseVector[Double] = {
+    val increment = (b - a) / (length - 1)
+    DenseVector.tabulate(length)(i => a + increment * i)
+  }
+
 }
 
