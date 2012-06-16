ae00600c1c5341cfa28fbf4dbde3435289746851
David Hall <dlwh@berkeley.edu>
2012-06-16 00:52:45 -0700

    new VectorSpace-type type classes.

--- math/src/main/scala/breeze/math/VectorSpace.scala
+++ math/src/main/scala/breeze/math/VectorSpace.scala
@@ -1,5 +1,10 @@
 package breeze.math
 
+import breeze.linalg.operators._
+import breeze.linalg.support.{CanNorm, CanCopy, CanCreateZerosLike}
+import breeze.linalg.NumericOps
+import breeze.generic.CanMapValues
+
 /**
  *
  * @tparam V Vector type
@@ -8,25 +13,65 @@ package breeze.math
  */
 trait VectorSpace[V, S] {
   def field: Field[S]
-  def zero: V
-  def timesVS(a: V, b: S):V
-  def divVS(a: V, b: S): V
 
-  def plusVV(a: V, b: V):V
-  def subVV(a: V, b: V):V
+  implicit def isNumericOps(v: V):NumericOps[V]
 
+  implicit def zeros: CanCreateZerosLike[V, V]
+  implicit def mulVS: BinaryOp[V, S, OpMulScalar, V]
+  implicit def divVS: BinaryOp[V, S, OpDiv, V]
+
+  implicit def addVV: BinaryOp[V, V, OpAdd, V]
+  implicit def subVV: BinaryOp[V, V, OpSub, V]
 
   def close(a: V, b: V, tolerance: Double):Boolean
 
   // default implementations
-  def negate(v: V) = timesVS(v, field.negate(field.one))
+  implicit def neg: UnaryOp[V, OpNeg, V]
 
 }
 
 trait NormedVectorSpace[V, S] extends VectorSpace[V, S] {
-  def norm(a: V):S
+  def norm(a: V):Double
+  def close(a: V, b: V, tolerance: Double):Boolean = norm(a - b) < tolerance
 }
 
 trait InnerProductSpace[V, S] extends NormedVectorSpace[V, S] {
   def dot(a: V, b: V): S
+}
+
+trait MutableVectorSpace[V, S] extends VectorSpace[V, S] {
+  implicit def copy: CanCopy[V]
+  implicit def mulIntoVS: BinaryUpdateOp[V, S, OpMulScalar]
+  implicit def divIntoVS: BinaryUpdateOp[V, S, OpDiv]
+
+  implicit def addIntoVV: BinaryUpdateOp[V, V, OpAdd]
+  implicit def subIntoVV: BinaryUpdateOp[V, V, OpSub]
+}
+
+trait MutableNormedSpace[V, S] extends NormedVectorSpace[V, S] with MutableVectorSpace[V, S]
+trait MutableInnerProductSpace[V, S] extends InnerProductSpace[V, S] with MutableVectorSpace[V, S]
+
+
+trait CoordinateSpace[V, S] extends InnerProductSpace[V, S] {
+  implicit def norm: CanNorm[V]
+  implicit def mapValues: CanMapValues[V,S,S,V]
+
+  implicit def addVS: BinaryOp[V, S, OpAdd, V]
+  implicit def subVS: BinaryOp[V, S, OpSub, V]
+  implicit def mulVV: BinaryOp[V, S, OpMulScalar, V]
+  implicit def divVV: BinaryOp[V, S, OpDiv, V]
+  implicit def powVV: BinaryOp[V, V, OpDiv, V]
+  implicit def powVS: BinaryOp[V, S, OpDiv, V]
+}
+
+trait MutableCoordinateSpace[V, S] extends MutableInnerProductSpace[V, S] {
+  implicit def norm: CanNorm[V]
+  implicit def mapValues: CanMapValues[V,S,S,V]
+
+  implicit def addIntoVS: BinaryUpdateOp[V, S, OpAdd]
+  implicit def subIntoVS: BinaryUpdateOp[V, S, OpSub]
+  implicit def mulIntoVV: BinaryUpdateOp[V, S, OpMulScalar]
+  implicit def divIntoVV: BinaryUpdateOp[V, S, OpDiv]
+  implicit def powIntoVV: BinaryUpdateOp[V, V, OpDiv]
+  implicit def powIntoVS: BinaryUpdateOp[V, S, OpDiv]
 }
