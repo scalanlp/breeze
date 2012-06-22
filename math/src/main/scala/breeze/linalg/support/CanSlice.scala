a725cc67e80353d764c71cf0167c0dfbc076b93b
David Hall <dlwh@berkeley.edu>
2012-06-22 10:51:29 -0700

    more FunctionN removal from implicits

--- math/src/main/scala/breeze/linalg/support/CanSlice.scala
+++ math/src/main/scala/breeze/linalg/support/CanSlice.scala
@@ -4,10 +4,14 @@ package breeze.linalg.support
  *
  * @author dlwh
  */
-trait CanSlice[-From, -Slice, +To] extends ((From, Slice)=>To)
+trait CanSlice[-From, -Slice, +To] {
+ def apply(from: From, slice: Slice):To
+}
 
 /**
  *
  * @author dlwh
  */
-trait CanSlice2[-From, -Slice1, -Slice2, +To] extends ((From,Slice1,Slice2)=>To)
+trait CanSlice2[-From, -Slice1, -Slice2, +To] {
+  def apply(from: From, slice: Slice1, slice2: Slice2):To
+}
