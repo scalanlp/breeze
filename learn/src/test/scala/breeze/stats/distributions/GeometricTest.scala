ff9935fab136ea63f435e5286ad4b7fa89731f59
David Hall <dlwh@berkeley.edu>
2012-06-23 17:57:31 -0700

    maybe a slightly more stable geometric distribution? I don't know.

--- learn/src/test/scala/breeze/stats/distributions/GeometricTest.scala
+++ learn/src/test/scala/breeze/stats/distributions/GeometricTest.scala
@@ -41,11 +41,11 @@ class GeometricTest extends FunSuite with Checkers with MomentsTestBase[Int] wit
   }
 
   implicit def arbParameter = Arbitrary {
-    for(p <- arbitrary[Double].map{m => val r = math.abs(m % 1.0); if (r < 5E-2) 0.1 + r else r}) yield p
+    for(p <- arbitrary[Double].map{m =>.5 + (m % 1.0)/2.0}) yield p
   }
 
   implicit def arbDistr = Arbitrary {
-    for(p <- arbitrary[Double].map{m => .5 + (math.abs(m) % 1.0)/2.0}) yield new Geometric(p)
+    for(p <- arbitrary[Double].map{m => .5 + (m % 1.0)/2.0}) yield new Geometric(p)
   }
 
 
