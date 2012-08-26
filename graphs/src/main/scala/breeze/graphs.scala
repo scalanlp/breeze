8c7221be0790def476338ffad307fb6e0308322a
David Hall <dlwh@berkeley.edu>
2012-08-26 15:09:54 -0700

    graphs is causing problems with its semiring stuff. I'll bring it back when i have time.

--- graphs/src/main/scala/breeze/graphs.scala
+++
@@ -1,9 +0,0 @@
-package breeze;
-
-/**
- * 
- * @author dlwh
- */
-package object graphs extends breeze.graphs.Search with breeze.graphs.Transformations
-                              with breeze.graphs.WeightedGraphs {
-}
