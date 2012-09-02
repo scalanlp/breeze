a4951eaf1bd41aaa717a7e60bd437df0c7c82fb8
David Hall <dlwh@berkeley.edu>
2012-09-02 12:45:43 -0700

    small change to Observation

--- process/src/main/scala/breeze/data/Observation.scala
+++ process/src/main/scala/breeze/data/Observation.scala
@@ -1,9 +1,9 @@
-package breeze.data;
+package breeze.data
 
 /*
  Copyright 2009 David Hall, Daniel Ramage
  
- Licensed under the Apache License, Version 2.0 (the "License");
+ Licensed under the Apache License, Version 2.0 (the "License")
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at 
  
@@ -17,7 +17,7 @@ package breeze.data;
 */
 
 
-import scala.collection.Map;
+import scala.collection.Map
 
 /**
 * Represents a single unlabeled example from a collection of data. Intentionally overly general.
@@ -25,21 +25,21 @@ import scala.collection.Map;
 * @author dlwh
 */
 trait Observation[+T] extends Serializable { outer=>
-  def id : String;
-  def features: T;
+  def id : String
+  def features: T
 
   /**
   * strict, but cached, transformation of features
   */
-  def map[U](f: T=>U) = new Observation[U] {
-    def id = outer.id;
-    val features = f(outer.features);
+  def map[U](f: T=>U):Observation[U] = new Observation[U] {
+    def id = outer.id
+    val features = f(outer.features)
   }
 
   /**
   * non-strict, but cached, transformation of features
   */
-  def flatMap[U](f: T=>U) = map(f);
+  def flatMap[U](f: T=>U) = map(f)
 
   override def toString = {
     "Observation { ids =" + id + ", features = " + features + "}"; 
@@ -48,13 +48,13 @@ trait Observation[+T] extends Serializable { outer=>
 
 object Observation {
   def apply[T](_id: String, _features: T) = new Observation[T] {
-   val id = _id;
-   val features = _features;
+   val id = _id
+   val features = _features
   }
 
   /**
   * Lifts a function to operate over Observations,
   * Rather than the contained object.
   */
-  def lift[T,U](f: T=>U) = (o : Observation[T]) => o.map(f);
+  def lift[T,U](f: T=>U) = (o : Observation[T]) => o.map(f)
 }
