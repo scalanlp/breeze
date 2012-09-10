caa7af1654b6bd68d2034017f8347751d759cb8c
David Hall <dlwh@berkeley.edu>
2012-09-10 16:51:19 -0700

    removed a deprecation warning

--- learn/src/test/scala/breeze/optimize/LBFGSTest.scala
+++ learn/src/test/scala/breeze/optimize/LBFGSTest.scala
@@ -76,7 +76,7 @@ class LBFGSTest extends OptimizeTestBase {
       val targetValue = 3 / (1.0 / 2 + 1)
       val result = lbfgs.minimize(DiffFunction.withL2Regularization(f, 1.0),init)
       val ok = norm(result :- DenseVector.ones[Double](init.size) * targetValue,2)/result.size < 3E-3
-      ok || error(init.toString + " " + result)
+      ok || (throw new RuntimeException("Failed to find optimum for init " + init))
     }
 
     check(Prop.forAll(optimizeThis _))
