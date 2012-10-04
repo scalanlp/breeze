c31be7843f9cbab017a284a3efa36185f81d7782
David Hall <dlwh@berkeley.edu>
2012-10-03 17:23:55 -0700

    i am actually an idiot.

--- math/src/main/scala/breeze/linalg/VectorBuilder.scala
+++ math/src/main/scala/breeze/linalg/VectorBuilder.scala
@@ -95,7 +95,7 @@ class VectorBuilder[@spec(Double,Int, Float) E](private var _index: Array[Int],
   def add(i: Int, v: E) {
     if(i < 0 || i > size) throw new IndexOutOfBoundsException(i + " not in [0,"+size+")")
 
-    if(_data.length >= used) {
+    if(_data.length <= used) {
       _data = ArrayUtil.copyOf(_data, math.max(_data.length * 2, 1))
       _index = ArrayUtil.copyOf(_index, math.max(_index.length * 2, 1))
     }
