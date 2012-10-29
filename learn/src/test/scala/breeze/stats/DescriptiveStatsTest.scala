945856bf76025ab03484b3ca3f4bb28128fd27ab
Marc Millstone <marc.millstone@socrata.com>
2012-10-29 11:33:01 -0700

    Another edge case when computing variance and the list is a singleton. Probably better to return 0 as opposed to NaN. (i.e. Population Variance as opposed to Standard Variance)

--- learn/src/test/scala/breeze/stats/DescriptiveStatsTest.scala
+++ learn/src/test/scala/breeze/stats/DescriptiveStatsTest.scala
@@ -2,12 +2,20 @@ package breeze.stats
 
 import org.scalatest.WordSpec
 import org.scalatest.matchers.ShouldMatchers
-class DescriptiveStatsTest extends WordSpec with ShouldMatchers{
-"The Percentile" should {
-  "not explode when p = 1" in {
-    val a = List.fill(100)(1.)
-    DescriptiveStats.percentile(a,1.) should be (1.)
+
+
+
+class DescriptiveStatsTest extends WordSpec with ShouldMatchers {
+  "DescriptiveStats" should {
+    "percentile should not explode when p = 1" in {
+      val a = List.fill(100)(1.0)
+      DescriptiveStats.percentile(a,1.) should be (1.0)
+    }
+    "variance should not explode when size of list is 1" in {
+      val a = List(1.)
+      DescriptiveStats.meanAndVariance(a) should be ((1.0,0))
+    }
   }
-}
+
 
 }
