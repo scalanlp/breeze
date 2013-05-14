5c3c0e4578209dddc8ca1227dc8a0eebf7a11bcd
Kenta Takagaki <kentaroh.takagaki+git@gmail.com>
2013-05-14 03:11:25 +0200

    start test file

---
+++ math/src/test/scala/breeze/signal/SignalTest.scala
@@ -0,0 +1,43 @@
+package breeze.signal
+
+import org.junit.runner.RunWith
+import org.scalatest._
+import org.scalatest.junit._
+import breeze.linalg._
+import breeze.math._
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: takagaki
+ * Date: 14.05.13
+ * Time: 02:31
+ * To change this template use File | Settings | File Templates.
+ */
+@RunWith(classOf[JUnitRunner])
+class SignalTest extends FunSuite {
+
+  val testNormThreshold = 1E-12
+
+  val testReal16 = DenseVector[Double](0.814723686393179, 0.905791937075619, 0.126986816293506, 0.913375856139019,
+  0.632359246225410, 0.0975404049994095, 0.278498218867048, 0.546881519204984, 0.957506835434298, 0.964888535199277,
+  0.157613081677548, 0.970592781760616, 0.957166948242946, 0.485375648722841, 0.800280468888800, 0.141886338627215)
+
+  val testReal16fft = DenseVector[Complex](
+    Complex(9.75146832375171, 0.00000000000000),     Complex(-0.0977261644584599, 0.994224442620027),
+    Complex(0.248156703823313, - 0.961542739609668), Complex(-0.973134608372272, - 0.424078607189411),
+    Complex(1.99837813056893, 0.119139969734688),    Complex(-0.00703114442532593, - 0.555686817611600),
+    Complex(0.117251950894930, - 2.54990031917926),  Complex(0.506759321091583, - 0.436614575872306),
+    Complex(-0.301197719706247, 0.00000000000000),   Complex(0.506759321091583, 0.436614575872306),
+    Complex(0.117251950894930, 2.54990031917926),    Complex(-0.00703114442532593, 0.555686817611600),
+    Complex(1.99837813056893, - 0.119139969734688),  Complex(-0.973134608372272, 0.424078607189411),
+    Complex(0.248156703823313, 0.961542739609668),   Complex(-0.0977261644584599, - 0.994224442620027)
+  )
+
+  test("fft 1D of Double (A)") {
+    assert( norm( fft(testReal16) - testReal16fft ) < testNormThreshold )
+  }
+
+
+
+}
