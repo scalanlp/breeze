ae68088117236cd3bbc6804813cb27b6228cfdc4
David Hall <dlwh@cs.berkeley.edu>
2013-06-01 14:44:40 -0700

    no more semicolons in DescriptiveStats

--- math/src/main/scala/breeze/stats/DescriptiveStats.scala
+++ math/src/main/scala/breeze/stats/DescriptiveStats.scala
@@ -1,12 +1,12 @@
 package breeze.stats
 
 import util.Sorting
-;
+
 
 /*
  Copyright 2009 David Hall, Daniel Ramage
 
- Licensed under the Apache License, Version 2.0 (the "License");
+ Licensed under the Apache License, Version 2.0 (the "License")
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
 
@@ -28,24 +28,24 @@ object DescriptiveStats {
   * Returns the mean and variance of an iterator in a pair.
   */
   def meanAndVariance[/*@specialized(Double, Float)*/ T ](it: TraversableOnce[T])(implicit frac: Fractional[T]) = {
-    import frac.mkNumericOps;
+    import frac.mkNumericOps
     val (mu,s,n) = it.foldLeft( (frac.zero,frac.zero,0)) { (acc,y) =>
-      val (oldMu,oldVar,n) = acc;
-      val i = n+1;
-      val d = y - oldMu;
-      val mu = oldMu + frac.one/frac.fromInt(i) * d;
-      val s = oldVar + frac.fromInt(i-1) / frac.fromInt(i) * d *d;
-      (mu,s,i);
+      val (oldMu,oldVar,n) = acc
+      val i = n+1
+      val d = y - oldMu
+      val mu = oldMu + frac.one/frac.fromInt(i) * d
+      val s = oldVar + frac.fromInt(i-1) / frac.fromInt(i) * d *d
+      (mu,s,i)
     }
-    (mu,if(n==1) frac.zero else s/frac.fromInt(n-1));
+    (mu,if(n==1) frac.zero else s/frac.fromInt(n-1))
   }
 
   /**
   * Returns the mean of the sequence of numbers
   */
   def mean[/*@specialized(Double, Float)*/ T](it : TraversableOnce[T])(implicit frac: Fractional[T]) = {
-    val (sum,n) = accumulateAndCount(it);
-    frac.div(sum , frac.fromInt(n));
+    val (sum,n) = accumulateAndCount(it)
+    frac.div(sum , frac.fromInt(n))
   }
 
   def variance[/*@specialized(Double, Float)*/ T](it : TraversableOnce[T])(implicit n: Fractional[T]) = meanAndVariance(it)._2
@@ -54,12 +54,12 @@ object DescriptiveStats {
    * Return the total sum and the number of T's
    */
   def accumulateAndCount[@specialized T](it : TraversableOnce[T])(implicit n: Numeric[T]) = it.foldLeft( (n.zero,0) ) { (tup,d) =>
-    import n.mkNumericOps;
-    (tup._1 + d, tup._2 + 1);
+    import n.mkNumericOps
+    (tup._1 + d, tup._2 + 1)
   }
 
   /**
-   * returns the estimate of it at p * it.size, where p in [0,1]
+   * returns the estimate of the data at p * it.size, where p in [0,1]
    */
   def percentile(it: TraversableOnce[Double], p: Double) = {
     if(p > 1 || p < 0) throw new IllegalArgumentException("p must be in [0,1]")
