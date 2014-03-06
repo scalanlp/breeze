8b19706685c869b9e449459dd0e617de7bced308
Chris Stucchio <stucchio@gmail.com>
2014-03-06 12:06:44 +0530

    More and better benchmarks of dense vector

--- benchmark/src/main/scala/breeze/Benchmark.scala
+++ benchmark/src/main/scala/breeze/Benchmark.scala
@@ -21,8 +21,26 @@ trait BreezeBenchmark extends SimpleBenchmark {
    * Sugar to run 'f' for 'reps' number of times.
    */
   def run[A](reps:Int)(f: => A): A = {
-    def loop(a: A, i: Int): A = if (i < reps) loop(f, i + 1) else a
-    if (reps < 1) sys.error("!") else loop(f, 1)
+    if (reps < 1) sys.error("!")
+    var i=0
+    var result: Option[A] = None
+    while (i < reps) {
+      result = Some(f)
+      i += 1
+    }
+    result.get
+  }
+
+  def runWith[A,B](reps: Int, constructor: =>B)(f: B=>A): A = {
+    if (reps < 1) sys.error("!")
+    var i=0
+    var result: Option[A] = None
+    val obj: B = constructor
+    while (i < reps) {
+      result = Some(f(obj))
+      i += 1
+    }
+    result.get
   }
 }
 
--- benchmark/src/main/scala/breeze/linalg/DenseVectorBenchmark.scala
+++ benchmark/src/main/scala/breeze/linalg/DenseVectorBenchmark.scala
@@ -9,7 +9,10 @@ object DenseVectorBenchmark extends MyRunner(classOf[DenseVectorBenchmark])
 
 trait BuildsRandomVectors {
   private val uniform = Uniform(0,1)
-  def randomArray(size: Int): DenseVector[Double] = {
+  def randomArray(size: Int, offset: Int = 0, stride: Int = 1): DenseVector[Double] = {
+    require(offset >= 0)
+    require(stride >= 1)
+    val v = new DenseVector(new Array[Double](offset+stride*size), offset, stride, size)
     val result = DenseVector.zeros[Double](size)
     var i=0;
     while (i < size) {
@@ -28,24 +31,54 @@ class DenseVectorBenchmark extends BreezeBenchmark with BuildsRandomVectors {
     DenseVector.fill[Double](1024, 23)
   }
 
-  private val applyArray = randomArray(1024*8)
-  def timeUnsafeValueAt(reps: Int) = run(reps) {
+  private def valueAtBench(reps: Int, size: Int, stride: Int) = runWith(reps, {randomArray(size, stride=stride)})(arr => {
     var i=0;
     var t: Double = 0
-    while (i < applyArray.size) {
-      t += applyArray.unsafeValueAt(i) //This is not strictly part of the benchmark, but done so that the JIT doensn't eliminate everything
+    while (i < arr.size) {
+      t += arr.valueAt(i) //This is not strictly part of the benchmark, but done so that the JIT doensn't eliminate everything
       i += 1
     }
     t
-  }
+  })
 
-  def timeApply(reps: Int) = run(reps) {
+  def timeValueAt(reps: Int) = valueAtBench(reps, 1024*8, 1)
+  def timeValueAtStride4(reps: Int) = valueAtBench(reps, 1024*8, 4)
+
+  private def unsafeValueAtBench(reps: Int, size: Int, stride: Int) = runWith(reps, {randomArray(size, stride=stride)})(arr => {
     var i=0;
     var t: Double = 0
-    while (i < applyArray.size) {
-      t += applyArray(i) //This is not strictly part of the benchmark, but done so that the JIT doensn't eliminate everything
+    while (i < arr.size) {
+      t += arr.unsafeValueAt(i) //This is not strictly part of the benchmark, but done so that the JIT doensn't eliminate everything
       i += 1
     }
     t
-  }
+  })
+
+  def timeUnsafeValueAt(reps: Int) = unsafeValueAtBench(reps, 1024*8, 1)
+  def timeUnsafeValueAtStride4(reps: Int) = unsafeValueAtBench(reps, 1024*8, 4)
+
+  private def updateBench(reps: Int, size: Int, stride: Int) = runWith(reps, {randomArray(size, stride=stride)})(arr => {
+    var i=0;
+    while (i < arr.size) {
+      arr.update(i, i.toDouble)
+      i += 1
+    }
+    arr
+  })
+
+  def timeUpdate(reps: Int) = updateBench(reps, 1024*8, 1)
+  def timeUpdateStride4(reps: Int) = updateBench(reps, 1024*8, 4)
+
+  private def unsafeUpdateBench(reps: Int, size: Int, stride: Int) = runWith(reps, {randomArray(size, stride=stride)})(arr => {
+    var i=0;
+    while (i < arr.size) {
+      arr.unsafeUpdate(i, i.toDouble)
+      i += 1
+    }
+    arr
+  })
+
+  def timeUnsafeUpdate(reps: Int) = unsafeUpdateBench(reps, 1024*8, 1)
+  def timeUnsafeUpdateStride4(reps: Int) = unsafeUpdateBench(reps, 1024*8, 4)
+
 }
