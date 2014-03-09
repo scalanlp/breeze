59c86c83756e182040729f708cf2b4b387939e52
Chris Stucchio <stucchio@gmail.com>
2014-03-09 13:08:37 +0530

    weird, seemingly nonsensical performance hack

--- src/main/scala/breeze/linalg/DenseVector.scala
+++ src/main/scala/breeze/linalg/DenseVector.scala
@@ -75,7 +75,10 @@ class DenseVector[@spec(Double, Int, Float) E](val data: Array[E],
     data(offset + trueI * stride) = v
   }
 
-  def unsafeUpdate(i: Int, v: E) = { data(offset + i * stride) = v }
+  //I have no fucking clue why this speeds things up, but it does seem to.
+  private final val innerUpdate: ((Int,E) => Unit) = if ((offset == 0) && (stride == 1)) { (i:Int,v:E) => {data(i) = v} } else {(i:Int,v:E) => {data(offset+i*stride)=v}  }
+
+  def unsafeUpdate(i: Int, v: E) = innerUpdate(i,v) //data(offset + i * stride) = v
 
   def activeIterator = iterator
 
