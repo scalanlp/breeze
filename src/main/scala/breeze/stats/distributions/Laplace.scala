da5d712318bfadada3caf1b25e6dc7da72508b81
David Hall <dlwh@cs.berkeley.edu>
2014-03-13 21:30:59 -0700

    laplace distribution

---
+++ src/main/scala/breeze/stats/distributions/Laplace.scala
@@ -0,0 +1,52 @@
+package breeze.stats.distributions
+
+import breeze.numerics.constants.{γ, Pi}
+import breeze.numerics.{exp, log}
+
+/**
+ * http://en.wikipedia.org/wiki/Laplace_distribution
+ *
+ * @author dlwh
+ **/
+case class Laplace(location: Double, scale: Double)(implicit rand: RandBasis = Rand) extends ContinuousDistr[Double] with Moments[Double, Double] with HasCdf {
+  def mean: Double = location
+
+  def mode: Double = location
+
+  def variance: Double = 2 * scale * scale
+
+  def entropy: Double = 1 + log(2 * scale)
+
+  def logNormalizer: Double = 2 * scale
+
+  /**
+   * Gets one sample from the distribution. Equivalent to sample()
+   */
+  def draw(): Double = {
+    // from numpy
+    val u = rand.uniform.draw()
+    if(u < 0.5) {
+      location + scale * log(2 * u)
+    } else {
+      location - scale * log(2 *(1 - u))
+    }
+  }
+
+  def unnormalizedLogPdf(x: Double): Double = {
+    -math.abs(x-location)/scale
+  }
+
+  def probability(x: Double, y: Double): Double = {
+    cdf(y) - cdf(x)
+  }
+
+  private def cdf(x: Double) = x match {
+    case Double.NegativeInfinity => 0.0
+    case Double.PositiveInfinity => 1.0
+    case x if x < location =>
+      0.5 * exp(unnormalizedLogPdf(x))
+    case x =>
+      1 - 0.5 * exp(unnormalizedLogPdf(x))
+  }
+
+}
---
+++ src/test/scala/breeze/stats/distributions/LaplaceTest.scala
@@ -0,0 +1,44 @@
+package breeze.stats.distributions
+
+/*
+ Copyright 2009 David Hall, Daniel Ramage
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+import org.scalatest._;
+import org.scalatest.junit._;
+import org.scalatest.prop._;
+import org.scalacheck._;
+import org.junit.runner.RunWith
+import org.apache.commons.math3.random.MersenneTwister
+
+@RunWith(classOf[JUnitRunner])
+class LaplaceTest extends FunSuite with Checkers with MomentsTestBase[Double] {
+  import Arbitrary.arbitrary
+
+
+  override val numSamples = 40000
+
+  def asDouble(x: Double) = x
+
+  def fromDouble(x: Double) = x
+
+  implicit def arbDistr = Arbitrary {
+    for(location <- arbitrary[Double].map{x => math.abs(x) % 1000.0 + 1.1}; // Laplace pdf at 0 not defined when location == 1
+        scale <- arbitrary[Double].map {x => math.abs(x) % 8.0 + 1.0}) yield new Laplace(location,scale)(new RandBasis(new MersenneTwister(0)))
+  }
+
+
+
+}
