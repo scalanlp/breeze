e24f057e7d27e32310eed1aa02bcd722cb7a5f33
ktakagaki <kentaroh.takagaki@gmail.com>
2014-04-17 16:38:25 +0200

    organize

--- src/test/scala/breeze/linalg/functions/padTest.scala
+++ src/test/scala/breeze/linalg/functions/padTest.scala
@@ -22,6 +22,7 @@ class padTest extends FunSuite {
     assert( padRight(testDVI, dimensions = 3                  ) == DenseVector(1,2,3) )
     assert( padRight(testDVD, dimensions = 7    , mode = 11d  ) == DenseVector(1d,2d,3d,4d,5d,  11d,11d) )
     assert( padRight(testDVD, dimensions = ( 7 ), mode = 11d  ) == DenseVector(1d,2d,3d,4d,5d,  11d,11d) )
+    assert( padRight(testDVD, 7                 , 10d         ) == DenseVector(1d,2d,3d,4d,5d,  10d,10d) )
     assert( padRight(testDVD, 7                 , Value(10d)  ) == DenseVector(1d,2d,3d,4d,5d,  10d,10d) )
     assert( padRight(testDVI, 7                 , Min         ) == DenseVector(1,2,3,4,5,  1,1) )
     assert( padRight(testDVI, 7                 , Max         ) == DenseVector(1,2,3,4,5,  5,5) )
@@ -31,9 +32,9 @@ class padTest extends FunSuite {
     assert( padRight(testDVI, 7                 , Reflect     ) == DenseVector(1,2,3,4,5,  5,4) )
 
     //This is a bit tricky to implement vis-a-vis BroadcastedRows
-    //assert( padRight(testDMI, OptPadDimensions.T2( Tuple2(7, 2) ), Value(100) ) == DenseVector(1,2,3,4,5,  100,100) )
+    //assert( padRight(testDMI, OptPadDimensions.T2( Tuple2(7, 3) ), 0 ) == DenseMatrix( (1,0,0),(2,0,0),(3,0,0),(4,0,0),(5,0,0),  (0,0,0), (0,0,0) )
 
-    //This is very tricky
+    //This is very tricky  (DenseVector ==padRight==> DenseMatrix)
     //assert( padRight(testDVI, (7, 2)) == DenseMatrix( (1,0),(2,0),(3,0),(4,0),(5,0),  (0,0), (0,0) )
 
 
