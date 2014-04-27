42c2e2522cf09259a34879e1c3b13b81176e410f
David Hall <dlwh@cs.berkeley.edu>
2014-04-26 23:56:44 -0700

    reorganize, breeze's main code is now in math/.
    This should make SBT happier.

--- math/src/main/scala/breeze/collection/mutable/RingBuffer.scala
+++ math/src/main/scala/breeze/collection/mutable/RingBuffer.scala
@@ -4,25 +4,25 @@ import scala.collection.mutable._
 import scala.collection.generic._
 
 class RingBuffer[A](m: Int) extends Buffer[A] with GenericTraversableTemplate[A, RingBuffer] with BufferLike[A, RingBuffer[A]] with Builder[A, List[A]] {
-  val buf = new ListBuffer[A]
+  private val buf = new ListBuffer[A]
 
-  private def resize: Unit = while (buf.size > m) buf.remove(0)
+  private def resize(): Unit = while (buf.size > m) buf.remove(0)
 
   def length = buf.length
   override def apply(n: Int): A = buf.apply(n)
   def update(n: Int, x: A) = buf.update(n, x)
-  def +=(x: A): this.type = { buf.+=(x); resize; this }
+  def +=(x: A): this.type = { buf.+=(x); resize(); this }
   def clear() = buf.clear();
-  def +=:(x: A): this.type = { buf.+=:(x); resize; this }
+  def +=:(x: A): this.type = { buf.+=:(x); resize(); this }
   def insertAll(n: Int, seq: scala.collection.Traversable[A]): Unit = buf.insertAll(n, seq)
   override def remove(n: Int, count: Int) = buf.remove(n, count)
-  def result: List[A] = buf.result
+  def result(): List[A] = buf.result()
   override def toList: List[A] = buf.toList
   def prependToList(xs: List[A]): List[A] = buf.prependToList(xs)
   def remove(n: Int): A = buf.remove(n)
   override def -=(elem: A): this.type = { buf.-=(elem); this }
   override def iterator = buf.iterator
-  override def readOnly: List[A] = buf.readOnly
+//  override def readOnly: List[A] = buf.readOnly
   override def equals(that: Any): Boolean = buf.equals(that)
   override def clone(): RingBuffer[A] = new RingBuffer(m) ++= this
   override def stringPrefix: String = "RingBuffer"
