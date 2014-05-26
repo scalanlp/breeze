38a8982f305e35fee87649627e2f85a642e97a8a
Chris Stucchio <stucchio@gmail.com>
2014-05-26 08:33:40 +0530

    benchmark now tracks both a simple and a harder ufunc

--- benchmark/src/main/scala/breeze/linalg/MappingUfuncBenchmark.scala
+++ benchmark/src/main/scala/breeze/linalg/MappingUfuncBenchmark.scala
@@ -15,13 +15,26 @@ object addOne extends UFunc with MappingUFunc {
   implicit object expDoubleImpl extends Impl[Double, Double] { def apply(v: Double) = v+1 }
 }
 
+object harderUfunc extends UFunc with MappingUFunc {
+  //A custom stupid ufunc that is very fast to run
+  implicit object expDoubleImpl extends Impl[Double, Double] { def apply(v: Double) = (v+1)/(1+v*v) }
+}
+
 class MappingUfuncBenchmark extends BreezeBenchmark with BuildsRandomMatrices with BuildsRandomVectors {
   def timeMappingUfuncDenseMat(reps: Int) = runWith(reps, {randomMatrix(2048,2048)})((mat:DenseMatrix[Double]) => {
     addOne(mat)
   })
 
+  def timeMappingUfuncDenseMatHarder(reps: Int) = runWith(reps, {randomMatrix(2048,2048)})((mat:DenseMatrix[Double]) => {
+    harderUfunc(mat)
+  })
+
   def timeMappingUfuncDenseVec(reps: Int) = runWith(reps, {randomArray(2048*2048)})((arr:DenseVector[Double]) => {
     addOne(arr)
+  })
+
+  def timeMappingUfuncDenseVecHarder(reps: Int) = runWith(reps, {randomArray(2048*2048)})((arr:DenseVector[Double]) => {
+    harderUfunc(arr)
   })
 
   def timeMappingUfuncArray(reps: Int) = runWith(reps, {randomArray(2048*2048)})((arr:DenseVector[Double]) => {
@@ -29,6 +42,16 @@ class MappingUfuncBenchmark extends BreezeBenchmark with BuildsRandomMatrices wi
     var i=0
     while (i < data.size) {
       data(i) = addOne(data(i))
+      i += 1
+    }
+    data
+  })
+
+  def timeMappingUfuncArrayHarder(reps: Int) = runWith(reps, {randomArray(2048*2048)})((arr:DenseVector[Double]) => {
+    val data = arr.data
+    var i=0
+    while (i < data.size) {
+      data(i) = harderUfunc(data(i))
       i += 1
     }
     data
