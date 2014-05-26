107d94d1f05c30cbbd8fe4404066fd18283d7810
Chris Stucchio <stucchio@gmail.com>
2014-05-26 13:00:00 +0530

    indent fix

--- math/src/main/scala/breeze/linalg/DenseMatrix.scala
+++ math/src/main/scala/breeze/linalg/DenseMatrix.scala
@@ -68,7 +68,7 @@ final class DenseMatrix[@specialized(Int, Float, Double) V](val rows: Int,
   if (isTranspose && (majorStride < cols)) { throw new IndexOutOfBoundsException("MajorStride == " + majorStride + " is smaller than cols == " + cols + ", which is impossible") }
   if ((!isTranspose) && (majorStride < rows)) { throw new IndexOutOfBoundsException("MajorStride == " + majorStride + " is smaller than rows == " + rows + ", which is impossible") }
 
-if (data.size < linearIndex(rows-1, cols-1)) { throw new IndexOutOfBoundsException("Storage array has size " + data.size + " but indices can grow as large as " + linearIndex(rows-1,cols-1)) }
+  if (data.size < linearIndex(rows-1, cols-1)) { throw new IndexOutOfBoundsException("Storage array has size " + data.size + " but indices can grow as large as " + linearIndex(rows-1,cols-1)) }
 
   def apply(row: Int, col: Int) = {
     if(row < - rows || row >= rows) throw new IndexOutOfBoundsException((row,col) + " not in [-"+rows+","+rows+") x [-"+cols+"," + cols+")")
