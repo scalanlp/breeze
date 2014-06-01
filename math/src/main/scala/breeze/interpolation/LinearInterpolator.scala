0012b1b0718c94f0f668311c6f2886d80bcf2f76
Christopher Medrela <chris.medrela@gmail.com>
2014-06-01 19:29:35 +0200

    working state

--- math/src/main/scala/breeze/interpolation/LinearInterpolator.scala
+++ math/src/main/scala/breeze/interpolation/LinearInterpolator.scala
@@ -7,11 +7,12 @@ package breeze.interpolation
 
 import scala.reflect.ClassTag
 
+import breeze.generic.{UFunc, MappingUFunc, VariableUFunc}
 import breeze.linalg._
 import breeze.linalg.support.CanMapValues
 import breeze.math.Field
 
-
+/*
 trait UnivariateInterpolator[T] {
   def apply(x: T): T
   def applyCol[ColIn, ColOut](x: ColIn)(implicit cmv: CanMapValues[ColIn, T, T, ColOut]) = cmv.map(x, apply _)
@@ -77,3 +78,80 @@ class LinearInterpolator[T:Ordering:ClassTag:Field]
         f.*(Y(index), w))
   }
 }
+*/
+
+object UnivariateInterpolatorImpl extends UFunc with MappingUFunc {
+  implicit object impl extends Impl2[UnivariateInterpolator,Double,Double] {
+    def apply(k: UnivariateInterpolator, v: Double) = k(v)
+  }
+}
+
+trait UnivariateInterpolator extends VariableUFunc[UnivariateInterpolatorImpl.type, UnivariateInterpolator] {
+  def apply(x: Double): Double
+}
+
+abstract class HandyUnivariateInterpolator
+    (x_coords: Vector[Double],
+     y_coords: Vector[Double])
+    extends UnivariateInterpolator {
+
+  if (x_coords.size != x_coords.toArray.toSet.size)
+    throw new Exception("x coordinates must be unique")
+  if (x_coords.size != y_coords.size)
+    throw new Exception("x_coords and y_coords must be of the same size")
+  if (x_coords.size == 0)
+    throw new Exception("need to provide at least one pair of coordinates")
+
+  private val nodes = x_coords.toArray zip y_coords.toArray sortBy {n => n._1}
+  protected val X: Array[Double] = nodes map {n => n._1}
+  protected val Y: Array[Double] = nodes map {n => n._2}
+
+  private val ord = implicitly[Ordering[Double]]
+  import ord.mkOrderingOps
+
+  def apply(x: Double): Double = {
+    if (x < X(0) || x > X(X.size - 1))
+      throw new IndexOutOfBoundsException("Out of the domain [" + X(0) + "," + X(X.size-1) + "]")
+
+    valueAt(x)
+  }
+
+  protected def valueAt(b: Double): Double
+}
+
+class LinearInterpolator
+    (x_coords: Vector[Double],
+     y_coords: Vector[Double])
+    extends HandyUnivariateInterpolator(x_coords, y_coords) {
+
+  private val ord = implicitly[Ordering[Double]]
+  import ord.mkOrderingOps
+
+  override protected def valueAt(x: Double): Double = {
+    def bisearch(low: Int, high: Int): Int = (low+high)/2 match {
+      case mid if low == high => mid
+      case mid if X(mid) < x => bisearch(mid+1, high)
+      case mid => bisearch(low, mid)
+    }
+    val index = bisearch(0, X.length-1)
+
+    val f = implicitly[Field[Double]]
+    // w = (x - x1) / (x2 - x1)
+    val w = f./(f.-(x,
+                    X(index-1)),
+                f.-(X(index),
+                    X(index-1)))
+    // u = 1 - w
+    val u = f.-(f.one, w)
+
+    // result = y1 * u + y2 * w
+    f.+(f.*(Y(index-1), u),
+        f.*(Y(index), w))
+  }
+}
+
+/*
+class MyInterpolatorUFuncWrapper2(val a: Double) extends UnivariateInterpolator {
+  def valueAt(b: Double) = a + b + 1
+}
+*/
--- math/src/test/scala/breeze/interpolation/LinearInterpolatorTest.scala
+++ math/src/test/scala/breeze/interpolation/LinearInterpolatorTest.scala
@@ -18,7 +18,7 @@ class LinearInterpolatorTest extends FunSuite {
     assert(f(1.5) == 3.0)
     assert(f(2.5) == 4.0)
     assert(f(2.25) == 4.5)
-    assert(f.applyCol(DenseVector(1.5, 2.25)) == DenseVector(3.0, 4.5))
+    assert(f(DenseVector(1.5, 2.25)) == DenseVector(3.0, 4.5))
   }
 
   test("unordered nodes") {
@@ -28,4 +28,21 @@ class LinearInterpolatorTest extends FunSuite {
     assert(f(1.5) == 3.5)
     assert(f(2.5) == 5.5)
   }
+
+  test("out of bounds") {
+    val x = DenseVector(1.0, 2.0)
+    val y = DenseVector(1.0, 2.0)
+    val f = new LinearInterpolator(x, y)
+    intercept[IndexOutOfBoundsException] {
+      f(0.5)
+    }
+  }
+
+  /*
+  test("MyTest2") {
+    assert(new MyInterpolatorUFuncWrapper(2.0)(2.0) == 4.0)
+    assert(new MyInterpolatorUFuncWrapper(2.0)(DenseVector(2.0, 3.0)) == DenseVector(4.0, 5.0))
+    assert(new MyInterpolatorUFuncWrapper2(1.0)(DenseVector(2.0, 3.0)) == DenseVector(4.0, 5.0))
+  }
+  */
 }
