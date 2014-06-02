3eab58f3c349692d448c4226fcf83c76e6126b7a
Christopher Medrela <chris.medrela@gmail.com>
2014-06-02 23:05:32 +0200

    not working

--- math/src/main/scala/breeze/interpolation/LinearInterpolator.scala
+++ math/src/main/scala/breeze/interpolation/LinearInterpolator.scala
@@ -13,20 +13,20 @@ import breeze.linalg.support.CanMapValues
 import breeze.math.Field
 
 
-object UnivariateInterpolatorImpl extends UFunc with MappingUFunc {
-  implicit object impl extends Impl2[UnivariateInterpolator,Double,Double] {
-    def apply(k: UnivariateInterpolator, v: Double) = k(v)
+object UnivariateInterpolatorImpl[T] extends UFunc with MappingUFunc {
+  implicit object impl extends Impl2[UnivariateInterpolator[T],Double,Double] {
+    def apply(k: UnivariateInterpolator[T], v: Double) = k(v)
   }
 }
 
-trait UnivariateInterpolator extends VariableUFunc[UnivariateInterpolatorImpl.type, UnivariateInterpolator] {
+trait UnivariateInterpolator[T] extends VariableUFunc[UnivariateInterpolatorImpl.type, UnivariateInterpolator[T]] {
   def apply(x: Double): Double
 }
 
-abstract class HandyUnivariateInterpolator
-    (x_coords: Vector[Double],
-     y_coords: Vector[Double])
-    extends UnivariateInterpolator {
+abstract class HandyUnivariateInterpolator[T]
+    (x_coords: Vector[T],
+     y_coords: Vector[T])
+    extends UnivariateInterpolator[T] {
 
   if (x_coords.size != x_coords.toArray.toSet.size)
     throw new Exception("x coordinates must be unique")
@@ -36,31 +36,31 @@ abstract class HandyUnivariateInterpolator
     throw new Exception("need to provide at least one pair of coordinates")
 
   private val nodes = x_coords.toArray zip y_coords.toArray sortBy {n => n._1}
-  protected val X: Array[Double] = nodes map {n => n._1}
-  protected val Y: Array[Double] = nodes map {n => n._2}
+  protected val X: Array[T] = nodes map {n => n._1}
+  protected val Y: Array[T] = nodes map {n => n._2}
 
-  private val ord = implicitly[Ordering[Double]]
+  private val ord = implicitly[Ordering[T]]
   import ord.mkOrderingOps
 
-  def apply(x: Double): Double = {
+  def apply(x: T): T = {
     if (x < X(0) || x > X(X.size - 1))
       throw new IndexOutOfBoundsException("Out of the domain [" + X(0) + "," + X(X.size-1) + "]")
 
     valueAt(x)
   }
 
-  protected def valueAt(b: Double): Double
+  protected def valueAt(b: T): T
 }
 
-class LinearInterpolator
-    (x_coords: Vector[Double],
-     y_coords: Vector[Double])
-    extends HandyUnivariateInterpolator(x_coords, y_coords) {
+class LinearInterpolator[T]
+    (x_coords: Vector[T],
+     y_coords: Vector[T])
+    extends HandyUnivariateInterpolator[T](x_coords, y_coords) {
 
-  private val ord = implicitly[Ordering[Double]]
+  private val ord = implicitly[Ordering[T]]
   import ord.mkOrderingOps
 
-  override protected def valueAt(x: Double): Double = {
+  override protected def valueAt(x: T): T = {
     def bisearch(low: Int, high: Int): Int = (low+high)/2 match {
       case mid if low == high => mid
       case mid if X(mid) < x => bisearch(mid+1, high)
@@ -68,7 +68,7 @@ class LinearInterpolator
     }
     val index = bisearch(0, X.length-1)
 
-    val f = implicitly[Field[Double]]
+    val f = implicitly[Field[T]]
     // w = (x - x1) / (x2 - x1)
     val w = f./(f.-(x,
                     X(index-1)),
@@ -84,6 +84,6 @@ class LinearInterpolator
 }
 
 object LinearInterpolator {
-  def apply(x_coords: Vector[Double],
-            y_coords: Vector[Double]) = new LinearInterpolator(x_coords, y_coords)
+  def apply[T](x_coords: Vector[T],
+               y_coords: Vector[T]) = new LinearInterpolator(x_coords, y_coords)
 }
