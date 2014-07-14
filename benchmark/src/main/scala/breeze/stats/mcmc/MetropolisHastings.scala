f5c1427ee2dfcc3979566e6800d79af826302884
Chris Stucchio <stucchio@gmail.com>
2014-07-14 12:29:17 +0530

    add test with work, to demonstrate threading speedup.

--- benchmark/src/main/scala/breeze/stats/mcmc/MetropolisHastings.scala
+++ benchmark/src/main/scala/breeze/stats/mcmc/MetropolisHastings.scala
@@ -13,7 +13,7 @@ class MetropolisHastingsBenchmark extends BreezeBenchmark {
   val burnIn = 1024*1024
   val dropCount = 25
   val numSamples = 1024*1024
-  val bufferSize = 1024*16
+  val bufferSize = 1024*32
 
   val epsilon = 1e-8
   def likelihood(x: Double) = 2*math.log(1+epsilon-x) + 3*math.log(x*x*x+epsilon) //Epsilon is present to avoid throwing exceptions in the unlikely event either 0 or 1 is sampled
@@ -22,6 +22,17 @@ class MetropolisHastingsBenchmark extends BreezeBenchmark {
     var result = 0.0
     cfor(0)(i => i<numSamples, i => i+1)(i => {
       result = m.draw()
+    })
+    result
+  }
+
+  def pullAllSamplesWithWork(m: Rand[Double]) = {
+    var result = 0.0
+    cfor(0)(i => i<numSamples, i => i+1)(i => {
+      val x = m.draw()
+      cfor(0)(j => j < 100, j => j+1)(j => {
+        result += math.log(math.exp(x)) / (1+x*x)
+      })
     })
     result
   }
@@ -41,9 +52,14 @@ class MetropolisHastingsBenchmark extends BreezeBenchmark {
     pullAllSamples(m)
   }
 
-  def timeThreadedBuffered(reps: Int) = run(reps) {
-    val m = ArbitraryThreadedBufferedMetropolisHastings[Double](likelihood _, (_:Double) => Uniform(0,1), init = 0.5, burnIn = burnIn, dropCount = dropCount, bufferSize=bufferSize)
-    val result = pullAllSamples(m)
+  def timeMetropolisHastingsWithWork(reps: Int) = run(reps) {
+    val m = ArbitraryMetropolisHastings(likelihood _, (_:Double) =>  Uniform(0,1), 0.5, burnIn=0, dropCount=dropCount)
+    pullAllSamplesWithWork(m)
+  }
+
+  def timeThreadedBufferedWithWork(reps: Int) = run(reps) {
+    val m = ArbitraryThreadedBufferedMetropolisHastings[Double](likelihood _, (_:Double) => Uniform(0,1), init = 0.5, burnIn = 0, dropCount = dropCount, bufferSize=bufferSize)
+    val result = pullAllSamplesWithWork(m)
     m.stop()
     result
   }
