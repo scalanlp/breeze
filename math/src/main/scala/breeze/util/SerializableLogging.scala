22478ca0f8863a220aa32a1f902eeb41773914e3
Xiangrui Meng <meng@databricks.com>
2014-08-02 18:20:51 -0700

    remove scalalogging

--- math/src/main/scala/breeze/util/SerializableLogging.scala
+++ math/src/main/scala/breeze/util/SerializableLogging.scala
@@ -1,8 +1,6 @@
 package breeze.util
 
 import org.slf4j.LoggerFactory
-import com.typesafe.scalalogging.slf4j.Logger
-
 
 /**
  * Stupid Typesafe logging lib trait isn't serializable. This is just a better version.
@@ -11,15 +9,15 @@ import com.typesafe.scalalogging.slf4j.Logger
  **/
 trait SerializableLogging extends Serializable {
   @transient @volatile
-  private var _the_logger:Logger = null
+  private var _the_logger: LazyLogger = null
 
-  def logger: Logger = {
+  protected def logger: LazyLogger = {
     var logger = _the_logger
     if(logger eq null) {
       synchronized {
         logger = _the_logger
         if(logger eq null) {
-          val ll = Logger(LoggerFactory.getLogger(this.getClass))
+          val ll = new LazyLogger(LoggerFactory.getLogger(this.getClass))
           _the_logger = ll
           logger = ll
         }
