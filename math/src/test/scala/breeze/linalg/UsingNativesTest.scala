eacccd8ffd299741ab9b26f5f421722d9f06acdb
Amit Kumar <akumar2@pandora.com>
2014-08-08 16:09:57 -0700

    using natives test

---
+++ math/src/test/scala/breeze/linalg/UsingNativesTest.scala
@@ -0,0 +1,35 @@
+package breeze.linalg
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import org.scalatest.prop.Checkers
+
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Amit Kumar kumaramit01@gmail.com
+ */
+@RunWith(classOf[JUnitRunner])
+class UsingNativesTest extends FunSuite with Checkers{
+
+import breeze.linalg._
+
+  test("Using natives") {
+    if(!usingNatives){
+      assert(com.github.fommil.netlib.BLAS.getInstance.getClass.getName == "com.github.fommil.netlib.F2jBLAS")
+    }else{
+      assert(com.github.fommil.netlib.BLAS.getInstance.getClass.getName != "com.github.fommil.netlib.F2jBLAS")
+    }
+  }
+
+}
