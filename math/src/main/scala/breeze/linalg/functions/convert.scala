430e2a06279938394cf22cd59b8291571ba02791
David Hall <dlwh@cs.berkeley.edu>
2014-08-08 09:28:40 -0700

    convert for Complex numbers

--- math/src/main/scala/breeze/linalg/functions/convert.scala
+++ math/src/main/scala/breeze/linalg/functions/convert.scala
@@ -2,6 +2,7 @@ package breeze.linalg
 
 import breeze.generic.{UFunc, MappingUFunc}
 import breeze.macros.expand
+import breeze.math.Complex
 
 /**
  * Provides casting facilities similar to Numpy's "astype" and Julia's "convert". Does casts of
@@ -16,8 +17,8 @@ object convert extends UFunc with MappingUFunc {
   @expand
   @expand.valify
   implicit def impl2[@expand.args(Int, Double, Float, Long, Char, Short) From,
-                        @expand.args(Int, Double, Float, Long, Char, Short) To]
-  (implicit @expand.sequence[To](_.toInt, _.toDouble, _.toFloat, _.toLong, _.toChar, _.toShort) conv: From=>To): Impl2[From, To.type, To] =  {
+                        @expand.args(Int, Double, Float, Long, Char, Short, Complex) To]
+  (implicit @expand.sequence[To](_.toInt, _.toDouble, _.toFloat, _.toLong, _.toChar, _.toShort, Complex(_, 0)) conv: From=>To): Impl2[From, To.type, To] =  {
     new Impl2[From, To.type, To] {
       def apply(v: From, v2: To.type): To = conv(v)
     }
