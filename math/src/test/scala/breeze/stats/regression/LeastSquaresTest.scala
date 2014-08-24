7a832408316f8f803f0b6f680ab79bb9013d6c81
Chris Stucchio <stucchio@gmail.com>
2014-08-24 17:28:47 -0400

    rename file

--- math/src/test/scala/breeze/stats/regression/LeastSquaresTest.scala
+++ math/src/test/scala/breeze/stats/regression/LeastSquaresTest.scala
@@ -6,16 +6,16 @@ import scala.util.Random
 import breeze.linalg._
 import spire.implicits.cfor
 
-class RegressionTest extends WordSpec with Matchers {
-  "Regression" should {
-    "do least squares for simple case" in {
+class LeastSquaresTest extends WordSpec with Matchers {
+  "Least squares" should {
+    "handle simple case" in {
       val a = DenseMatrix((1.0,1.0), (2.0, -2.0),(3.0, 3.0), (4.0, 5.0))
       val b = DenseVector(2.0, 0.0, 6.0, 9.0)
       val result = leastSquares(a,b)
       assert(norm(result.coefficients - DenseVector(1.0, 1.0)) < 1e-7)
       assert(result.rSquared < 1e-7)
     }
-    "do least squares for trickier case" in {
+    "handle trickier case" in {
       val a = new DenseMatrix[Double](100,2)
       val b = new DenseVector(new Array[Double](100))
       cfor(0)(i => i < 100, i => i+1)(i => {
