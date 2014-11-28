2495e77ce9740345fc0644c9a416e2609cc6edaa
David Hall <dlwh@cs.berkeley.edu>
2014-11-27 20:38:05 -0800

    array.tomultimap

--- math/src/test/scala/breeze/util/ImplicitsTest.scala
+++ math/src/test/scala/breeze/util/ImplicitsTest.scala
@@ -40,4 +40,9 @@ class ImplicitsTest extends FunSuite {
     assert(IndexedSeq( (1, 2), (1, 3), (1, 2), (2,4)).toMultiMap  === Map(1 -> IndexedSeq(2, 3, 2), 2 -> IndexedSeq(4)))
   }
 
+  test("Array#toMultiMap") {
+    assert(Array( (1, 2), (1, 3), (1, 2), (2,4)).toMultiMap.mapValues(_.deep)
+      === Map(1 -> Array(2, 3, 2), 2 -> Array(4)).mapValues(_.deep))
+  }
+
 }
