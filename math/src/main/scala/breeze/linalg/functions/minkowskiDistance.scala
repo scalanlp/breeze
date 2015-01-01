5c26a9bceb1fbd621421fa459e1b1202e91f5e9b
David Hall <dlwh@cs.berkeley.edu>
2015-01-01 15:07:45 -0800

    performance improvements to all distnace functions. minkowski still sucks

--- math/src/main/scala/breeze/linalg/functions/minkowskiDistance.scala
+++ math/src/main/scala/breeze/linalg/functions/minkowskiDistance.scala
@@ -2,14 +2,31 @@ package breeze.linalg.functions
 
 import breeze.generic.UFunc
 import breeze.linalg._
+import breeze.linalg.operators.OpSub
+import breeze.numerics.pow
 
 /**
  * A Minkowski distance metric implementation between two points
  */
-object minkowskiDistance extends UFunc {
+object minkowskiDistance extends UFunc with minkowskiDistanceLowPrio {
+  implicit def minkowskiDistanceFromSubtractionAndNorm[T, U, V]
+  (implicit sub: OpSub.Impl2[T, U, V],
+   powImpl: norm.Impl2[V, Double, V],
+   sumImpl: sum.Impl[V, Double]): Impl3[T, U, Double, Double] = {
+
+    new Impl3[T, U, Double, Double] {
+      def apply(v: T, v2: U, exponent: Double): Double = {
+        pow(sum(powImpl(sub(v, v2), exponent)), 1/exponent)
+      }
+    }
+  }
+
+}
+
+sealed trait minkowskiDistanceLowPrio { this: minkowskiDistance.type =>
 
   implicit def minkowskiDistanceFromZippedValues[T, U]
-      (implicit zipImpl: zipValues.Impl2[T, U, ZippedValues[Double, Double]]): Impl3[T, U, Double, Double] = {
+  (implicit zipImpl: zipValues.Impl2[T, U, ZippedValues[Double, Double]]): Impl3[T, U, Double, Double] = {
 
     new Impl3[T, U, Double, Double] {
       def apply(v: T, v2: U, exponent: Double): Double = {
--- math/src/test/scala/breeze/linalg/functions/minkowskiDistanceTest.scala
+++ math/src/test/scala/breeze/linalg/functions/minkowskiDistanceTest.scala
@@ -22,4 +22,19 @@ class minkowskiDistanceTest extends FunSuite {
     val v2 = SparseVector(9.0, -9)
     assert(minkowskiDistance(v1, v2, 3.0) === 7.872994366204345)
   }
+
+  /*
+  test("big sparse vectors shouldn't be insanely inefficient") {
+    val v1 = SparseVector.zeros[Double](100000)
+    val v2 = SparseVector.zeros[Double](10)
+    // TODO: how better to measure this
+    val in = System.currentTimeMillis()
+    assert(minkowskiDistance(v2, v2, 3.0) === 0.0)
+    val outSmall = System.currentTimeMillis()
+    assert(minkowskiDistance(v1, v1, 3.0) === 0.0)
+    val outBig = System.currentTimeMillis()
+    assert((outBig - outSmall) <= 2 * (outSmall - in), s"too slow! ${outBig - outSmall} ms vs ${outSmall - in} ms")
+
+  }
+  */
 }
