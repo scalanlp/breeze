d50577d139c9921af91817bcb9ab7835df9b0ab3
David Hall <dlwh@cs.berkeley.edu>
2015-01-02 14:21:26 -0800

    bugfix/performance fix to minkowski distance

--- math/src/test/scala/breeze/linalg/functions/minkowskiDistanceTest.scala
+++ math/src/test/scala/breeze/linalg/functions/minkowskiDistanceTest.scala
@@ -8,22 +8,21 @@ class minkowskiDistanceTest extends FunSuite {
   test("it should be correct with dense vectors") {
     val v1 = DenseVector(1.0, 1.0)
     val v2 = DenseVector(9.0, -9.0)
-    assert(minkowskiDistance(v1, v2, 3.0) === 7.872994366204345)
+    assert(minkowskiDistance(v1, v2, 3.0) === 11.477587096634332)
   }
 
   test("it should be correct with sparse vectors") {
     val v1 = SparseVector(1.0, 1.0)
     val v2 = SparseVector(9.0, -9.0)
-    assert(minkowskiDistance(v1, v2, 3.0) === 7.872994366204345)
+    assert(minkowskiDistance(v1, v2, 3.0) === 11.477587096634332)
   }
 
   test("it should be correct with dense vector and sparse vector") {
     val v1 = DenseVector(1, 1.0)
     val v2 = SparseVector(9.0, -9)
-    assert(minkowskiDistance(v1, v2, 3.0) === 7.872994366204345)
+    assert(minkowskiDistance(v1, v2, 3.0) === 11.477587096634332)
   }
 
-  /*
   test("big sparse vectors shouldn't be insanely inefficient") {
     val v1 = SparseVector.zeros[Double](100000)
     val v2 = SparseVector.zeros[Double](10)
@@ -36,5 +35,4 @@ class minkowskiDistanceTest extends FunSuite {
     assert((outBig - outSmall) <= 2 * (outSmall - in), s"too slow! ${outBig - outSmall} ms vs ${outSmall - in} ms")
 
   }
-  */
 }
