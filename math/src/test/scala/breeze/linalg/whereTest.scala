4935c0df8eeb39a924689770ec1c9839d93d6f39
David Hall <dlwh@cs.berkeley.edu>
2015-01-15 12:27:45 -0800

    test to ensure that where works with bitvectors

--- math/src/test/scala/breeze/linalg/whereTest.scala
+++ math/src/test/scala/breeze/linalg/whereTest.scala
@@ -15,6 +15,10 @@ class whereTest extends FunSuite {
     assert(where(SparseVector(100)( 0 -> 1, 1 -> 2,3 -> 4, 4 -> 5)) === IndexedSeq(0,1,3,4))
   }
 
+  test("BitVector") {
+    assert(where(DenseVector(1,2,0,4,5,0) :== 0) === IndexedSeq(2,5))
+  }
+
 
   test("DenseVector 3 arg") {
     assert(where(DenseVector(1,2,0,4,5,0), DenseVector(1,2,3,4,5,6), DenseVector(-1,-2,-3,-4,-5,-6)) === DenseVector(1,2,-3,4,5,-6))
