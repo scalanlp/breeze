8f7df23bb2c9128677de024d936be66d46fd896e
T3L <t3l@threelights.de>
2015-04-29 19:22:41 +1000

    Added Float implementation for matrix-inverse.

--- math/src/main/scala/breeze/linalg/functions/inv.scala
+++ math/src/main/scala/breeze/linalg/functions/inv.scala
@@ -15,7 +15,7 @@ import breeze.linalg.support.CanTranspose
  * A \ B.
  */
 object inv extends UFunc {
-  implicit def canInvUsingLU[T](implicit luImpl: LU.Impl[T, (DenseMatrix[Double], Array[Int])]):Impl[T, DenseMatrix[Double]] = {
+  implicit def canInvUsingLU_Double[T](implicit luImpl: LU.Impl[T, (DenseMatrix[Double], Array[Int])]):Impl[T, DenseMatrix[Double]] = {
     new Impl[T, DenseMatrix[Double]] {
       def apply(X: T): DenseMatrix[Double] = {
         // Should these type hints be necessary?
@@ -39,6 +39,32 @@ object inv extends UFunc {
       }
     }
   }
+
+  implicit def canInvUsingLU_Float[T](implicit luImpl: LU.Impl[T, (DenseMatrix[Float], Array[Int])]):Impl[T, DenseMatrix[Float]] = {
+    new Impl[T, DenseMatrix[Float]] {
+      def apply(X: T): DenseMatrix[Float] = {
+        // Should these type hints be necessary?
+        val (m:DenseMatrix[Float], ipiv:Array[Int]) = LU(X)
+        val N         = m.rows
+        val lwork     = scala.math.max(1, N)
+        val work      = Array.ofDim[Float](lwork)
+        val info      = new intW(0)
+        lapack.sgetri(
+          N, m.data, scala.math.max(1, N) /* LDA */,
+          ipiv,
+          work /* workspace */, lwork /* workspace size */,
+          info
+        )
+        assert(info.`val` >= 0, "Malformed argument %d (LAPACK)".format(-info.`val`))
+
+        if (info.`val` > 0)
+          throw new MatrixSingularException
+
+        m
+      }
+    }
+  }
+
 }
 
 
