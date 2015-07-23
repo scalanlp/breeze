scalanlp/breeze

commit a81e0a055c0429d9bbaaee3ca5606673632f71c5

speed improvements to DenseVector's map, again

math/src/main/scala/breeze/linalg/DenseVector.scala
@@ -318,8 +318,14 @@ object DenseVector extends VectorConstructors[DenseVector]
 
         // https://wikis.oracle.com/display/HotSpotInternals/RangeCheckElimination
         if (stride == 1) {
-          cforRange(0 until arr.length) { j =>
-            arr(j) = fn(d(j + off))
+          if (off == 0) {
+            cforRange(0 until arr.length) { j =>
+              arr(j) = fn(d(j))
+            }
+          } else {
+            cforRange(0 until arr.length) { j =>
+              arr(j) = fn(d(j + off))
+            }
           }
         } else {
           var i = 0
@@ -393,13 +399,20 @@ object DenseVector extends VectorConstructors[DenseVector]
         val d = from.data
         val stride = from.stride
 
+        val offset = from.offset
         if (stride == 1)  {
-          cforRange(from.offset until from.offset + from.size) { j =>
-            d(j) = fn(d(j))
+          if (offset == 0) {
+            cforRange(0 until d.length) { j =>
+              d(j) = fn(d(j))
+            }
+          } else {
+            cforRange(offset until offset + from.size) { j =>
+              d(j - offset) = fn(d(j))
+            }
           }
         } else {
           var i = 0
-          var j = from.offset
+          var j = offset
           while(i < from.length) {
             d(j) = fn(d(j))
             i += 1

commit dd6eb992e974ec02f8c3f399cde57d201807d6f7

Fixed bug introduced in commit: https://github.com/scalanlp/breeze/commit/a81e0a055c0429d9bbaaee3ca5606673632f71c5

math/src/main/scala/breeze/linalg/DenseVector.scala
@@ -397,26 +397,20 @@ object DenseVector extends VectorConstructors[DenseVector]
     new CanTransformValues[DenseVector[V], V] {
       def transform(from: DenseVector[V], fn: (V) => V) {
         val d = from.data
+        val length = from.length
         val stride = from.stride
 
         val offset = from.offset
         if (stride == 1)  {
-          if (offset == 0) {
-            cforRange(0 until d.length) { j =>
-              d(j) = fn(d(j))
-            }
-          } else {
-            cforRange(offset until offset + from.size) { j =>
-              d(j - offset) = fn(d(j))
-            }
+          cforRange(offset until offset + length) { j =>
+            d(j) = fn(d(j))
           }
         } else {
-          var i = 0
-          var j = offset
-          while(i < from.length) {
-            d(j) = fn(d(j))
-            i += 1
-            j += stride
+          val end = offset + stride * length
+          var i = offset
+          while (i != end) {
+            d(i) = fn(d(i))
+            i += stride
           }
 
         }