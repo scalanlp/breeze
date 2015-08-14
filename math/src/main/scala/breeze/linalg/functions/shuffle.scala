203fd86db0707950a256cbce33e8de32f68ca488
David Hall <dlwh@cs.berkeley.edu>
2015-08-14 13:57:42 -0700

    quickSelect, shuffle for indexedSeqs

--- math/src/main/scala/breeze/linalg/functions/shuffle.scala
+++ math/src/main/scala/breeze/linalg/functions/shuffle.scala
@@ -3,51 +3,81 @@ package breeze.linalg
 import breeze.generic.UFunc
 import breeze.macros.expand
 import spire.math.Complex
+import scala.collection.generic.CanBuildFrom
+import scala.collection.mutable.ArrayBuffer
 import scala.reflect.ClassTag
 import breeze.stats.distributions.Rand
 
 /**Returns the given DenseVector, Array, or DenseMatrix as a shuffled copy. (Fisher-Yates shuffle)
- * @author ktakagaki
- * @date 05/12/2014.
- */
+  * @author ktakagaki
+  * @date 05/12/2014.
+  */
 object shuffle extends UFunc {
 
-  @expand
-  implicit def implShuffle_Arr_eq_Arr[@expand.args(Int, Double, Long, Float) T]: Impl[Array[T], Array[T]] =
-  new Impl[Array[T], Array[T]] {
+  implicit def implShuffle_Arr_eq_Arr[T](implicit ct: ClassTag[T]): Impl[Array[T], Array[T]] = {
+    new Impl[Array[T], Array[T]] {
 
-    protected val _classTag: ClassTag[T] = scala.reflect.classTag[T]
+      def apply(arr: Array[T]): Array[T] = {
+        val tempret = arr.clone()
+        val rand = Rand.randInt(tempret.length)
 
-    def apply(arr: Array[T]): Array[T] = {
-      //copy
-      val tempret = arr.clone()
-      val rand = Rand.randInt(tempret.length)
-
-      var count = 0
-      while(count < tempret.length) {
-        swap(tempret, count, rand.get())
-        count += 1
+        var count = 0
+        while(count < tempret.length) {
+          swap(tempret, count, rand.get())
+          count += 1
+        }
+        tempret
       }
-      tempret
-    }
 
-    def swap(arr: Array[T], indexA: Int, indexB: Int): Unit = {
-      val temp = arr(indexA)
-      arr(indexA) = arr(indexB)
-      arr(indexB) = temp
-    }
+      def swap(arr: Array[T], indexA: Int, indexB: Int): Unit = {
+        val temp = arr(indexA)
+        arr(indexA) = arr(indexB)
+        arr(indexB) = temp
+      }
 
+    }
   }
 
-  @expand
-  implicit def implShuffle_DV_eq_DV[@expand.args(Int, Double, Long, Float) T]: Impl[DenseVector[T], DenseVector[T]] =
-    new Impl[DenseVector[T], DenseVector[T]] {
-      def apply(dv: DenseVector[T]): DenseVector[T] = DenseVector( shuffle(dv.toArray) )
+
+
+  implicit def implShuffle_Coll_eq_Coll[Coll, T, CollRes](implicit view: Coll <:< IndexedSeq[T],
+                                                          cbf: CanBuildFrom[Coll, T, CollRes]) : Impl[Coll, CollRes] = {
+    new Impl[Coll, CollRes] {
+
+      override def apply(v: Coll): CollRes = {
+        val builder = cbf(v)
+        val copy = v.to[ArrayBuffer]
+        val rand = Rand.randInt(copy.length)
+
+        var count = 0
+        while (count < copy.length) {
+          swap(copy, count, rand.get())
+          count += 1
+        }
+
+        builder ++= copy
+        builder.result()
+      }
+
+      def swap(arr: ArrayBuffer[T], indexA: Int, indexB: Int): Unit = {
+        val temp = arr(indexA)
+        arr(indexA) = arr(indexB)
+        arr(indexB) = temp
+      }
+
     }
+  }
+
+  implicit def implShuffle_DV_eq_DV[T](implicit arrImpl: Impl[Array[T], Array[T]],
+                                       ct: ClassTag[T]): Impl[DenseVector[T], DenseVector[T]] = {
+    new Impl[DenseVector[T], DenseVector[T]] {
+      def apply(dv: DenseVector[T]): DenseVector[T] = DenseVector(shuffle(dv.toArray))
+    }
+  }
 
 
-  @expand
-  implicit def implShuffle_DM_eq_DM[@expand.args(Int, Double, Long, Float) T]: Impl[DenseMatrix[T], DenseMatrix[T]] =
+  implicit def implShuffle_DM_eq_DM[T](implicit arrImpl: Impl[Array[T], Array[T]],
+                                       ct: ClassTag[T]): Impl[DenseMatrix[T], DenseMatrix[T]] = {
     new Impl[DenseMatrix[T], DenseMatrix[T]] {
 
       def apply(dm: DenseMatrix[T]): DenseMatrix[T] = {
@@ -56,6 +86,7 @@ object shuffle extends UFunc {
         new DenseMatrix(rows, cols, shuffle(dm.toArray))
       }
     }
+  }
 
 
 }
