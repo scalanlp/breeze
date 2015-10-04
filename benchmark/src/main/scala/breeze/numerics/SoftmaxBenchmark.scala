c525485684b2acf4782565dbde81c5982424a1a9
David Hall <dlwh@semanticmachines.com>
2015-10-03 23:24:50 -0700

    softmax float impl

---
+++ benchmark/src/main/scala/breeze/numerics/SoftmaxBenchmark.scala
@@ -0,0 +1,25 @@
+package breeze.numerics
+
+import breeze.benchmark.{MyRunner, BreezeBenchmark}
+import breeze.linalg.{softmax, DenseVector}
+import breeze.stats.distributions.Rand
+import spire.syntax.cfor._
+
+/**
+ * Created by dlwh on 10/3/15.
+ */
+class SoftmaxBenchmark extends BreezeBenchmark {
+  val dv = DenseVector.rand(5000, Rand.uniform.map(_.toFloat))
+
+  def timeSoftmaxFloat(reps: Int) = {
+    var sum = 0.0
+    cforRange(0 until reps) { _ =>
+      sum += softmax(dv)
+    }
+
+    sum
+  }
+
+}
+
+object SoftmaxBenchmark extends MyRunner(classOf[SoftmaxBenchmark])
