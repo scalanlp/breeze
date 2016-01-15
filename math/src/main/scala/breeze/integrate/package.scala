3bfb543db775930b1aea65d60b0c31078d31e3de
Jake Timothy <jake.a.timothy@gmail.com>
2016-01-14 21:08:01 -0800

    moved ode package to integrate and simplified

--- math/src/main/scala/breeze/integrate/package.scala
+++ math/src/main/scala/breeze/integrate/package.scala
@@ -3,6 +3,7 @@ package breeze
 import breeze.linalg._
 
 package object integrate {
+  
   def trapezoid(f: Double => Double, start: Double, end: Double, nodes: Int) : Double = {
     if (nodes < 2)
       throw new Exception("When using trapezoid, you have to use at least two nodes.")
@@ -19,5 +20,27 @@ package object integrate {
     val h = (end-start) / (nodes-1)
     val s = sum(for (i <- 0 until nodes-1) yield f(start+(i+0.5)*h))
     trapezoid(f, start, end, nodes) / 3.0 + s * 2/3.0 * h
+  }
+
+  def ode45(
+    f: (DenseVector[Double], Double) => DenseVector[Double],
+    y0: DenseVector[Double],
+    t: Array[Double],
+    relTol: Array[Double] = Array.empty,
+    absTol: Array[Double] = Array.empty
+    ) : Array[DenseVector[Double]] = {
+    
+    RungeKuttaODESolver(DormandPrinceTableau, f, y0, t, relTol, absTol)
+  }
+
+  def ode23(
+    f: (DenseVector[Double], Double) => DenseVector[Double],
+    y0: DenseVector[Double],
+    t: Array[Double],
+    relTol: Array[Double] = Array.empty,
+    absTol: Array[Double] = Array.empty
+    ) : Array[DenseVector[Double]] = {
+    
+    RungeKuttaODESolver(BogackiShampineTableau, f, y0, t, relTol, absTol)
   }
 }
