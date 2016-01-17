7714bf1c82f3103d5e09106749de1df5c2953586
Jake Timothy <jake.a.timothy@gmail.com>
2016-01-16 23:15:58 -0800

    modified step update using scipy metric

--- math/src/main/scala/breeze/integrate/RungeKuttaButcherTableau.scala
+++ math/src/main/scala/breeze/integrate/RungeKuttaButcherTableau.scala
@@ -7,6 +7,7 @@ import breeze.linalg._
 *
 * @author jaketimothy
 */
+
 trait RungeKuttaButcherTableau {
 
   def stages: Int
@@ -45,7 +46,7 @@ object BogackiShampineTableau extends RungeKuttaButcherTableau {
     Array(0.0, 3.0/4.0),
     Array(2.0/9.0, 1.0/3.0, 4.0/9.0))
 
-  override val b = Array(2.0/9.0, 1.0/3.0, 4.0/9.0, 0.0)
+  override val b = a(2) :+ 0.0
   override val bStar = Array(7.0/24.0, 0.25, 1.0/3.0, 1.0/8.0)
   override val c = Array(0.5, 0.75, 1.0)
 
@@ -106,7 +107,7 @@ object DormandPrinceTableau extends RungeKuttaButcherTableau {
     Array(9017.0/3168.0, -355.0/33.0, 46732.0/5247.0, 49.0/176.0, -5103.0/18656.0),
     Array(35.0/384.0, 0.0, 500.0/1113.0, 125.0/192.0, -2187.0/6784.0, 11.0/84.0))
 
-  override val b = Array(35.0/384.0, 0.0, 500.0/1113.0, 125.0/192.0, -2187.0/6784.0, 11.0/84.0, 0.0)
+  override val b = a(5) :+ 0.0
   override val bStar = Array(5179.0/57600.0, 0.0, 7571.0/16695.0, 393.0/640.0, -92097.0/339200.0, 187.0/2100.0, 1.0/40.0)
   override val c = Array(0.2, 0.3, 0.8, 8.0/9.0, 1.0, 1.0)
 
