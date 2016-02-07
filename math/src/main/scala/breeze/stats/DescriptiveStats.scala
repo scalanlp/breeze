dc35f1296ea57b24784514972f15207828873a03
Chris Stucchio <stucchio@gmail.com>
2016-02-07 11:46:31 -0500

    Added sparse bincount

--- math/src/main/scala/breeze/stats/DescriptiveStats.scala
+++ math/src/main/scala/breeze/stats/DescriptiveStats.scala
@@ -400,6 +400,52 @@ trait DescriptiveStats {
         }
       }
   }
+
+  /**
+    * A [[breeze.generic.UFunc]] for counting bins.
+    *
+    * This differs from bincount in that the result it returns is a SparseVector.
+    * The internal implementation of this could probably be significantly sped
+    * up by avoiding the use of counter. Then again, in sparse situations it's
+    * still a lot faster than using bincount.
+    *
+    */
+  object bincountSparse extends UFunc {
+    import breeze.linalg._
+
+    @expand
+    implicit def vecVersion[@expand.args(Double, Complex, Float) T]: Impl2[DenseVector[Int], DenseVector[T], SparseVector[T]] =
+      new Impl2[DenseVector[Int], DenseVector[T], SparseVector[T]] {
+        def apply(x: DenseVector[Int], weights: DenseVector[T]): SparseVector[T] = {
+          require(min(x) >= 0)
+          require(x.length == weights.length)
+          val counter = Counter[Int,T]()
+          cfor(0)(i => i < x.length, i => i+1)(i => {
+            counter.update(x(i), counter(x(i)) + weights(i))
+          })
+          val sortedDataArray = counter.toMap.toArray.sortWith((x,y) => x._1 < y._1).toArray
+          SparseVector(sortedDataArray(sortedDataArray.size-1)._1+1)(sortedDataArray: _*)
+        }
+      }
+
+    implicit def reduce[T](implicit iter: CanTraverseValues[T, Int]): Impl[T, SparseVector[Int]] =
+      new Impl[T, SparseVector[Int]] {
+        def apply(x: T): SparseVector[Int] = {
+          require(min(x) >= 0)
+          val counter = Counter[Int,Int]()
+
+          class BincountVisitor extends ValuesVisitor[Int] {
+            def visit(a: Int): Unit = { counter.update(a,counter(a) + 1) }
+            def zeros(numZero: Int, zeroValue: Int) = {
+              counter.update(zeroValue, counter(zeroValue) + numZero)
+            }
+          }
+          iter.traverse(x, new BincountVisitor)
+          val sortedDataArray = counter.toMap.toArray.sortWith((x,y) => x._1 < y._1).toArray
+          SparseVector(sortedDataArray(sortedDataArray.size-1)._1+1)(sortedDataArray: _*)
+        }
+      }
+  }
 }
 
 /**
--- math/src/test/scala/breeze/stats/DescriptiveStatsTest.scala
+++ math/src/test/scala/breeze/stats/DescriptiveStatsTest.scala
@@ -108,6 +108,19 @@ class DescriptiveStatsTest extends WordSpec with Matchers {
       val result = DenseVector[Int](0,1,2,2,2,1)
       assert(result == bincount(x))
     }
+
+    "bincountSparse should compute bins for DenseVector" in {
+      val x = DenseVector[Int](0,10,20,300,10)
+      val result = new SparseVector[Int](Array[Int](0,10,20,300), Array[Int](1, 2, 1, 1), 301)
+      assert(result == bincountSparse(x))
+    }
+
+    "bincountSparse should compute weighted bins for DenseVector" in {
+      val x = DenseVector[Int](0,10,20,300,10)
+      val weights = DenseVector[Double](1.0,3.0, 1.0, 7.0, 1.0)
+      val result = new SparseVector[Double](Array[Int](0,10,20,300), Array[Double](1.0, 4.0, 1.0, 7.0), 301)
+      assert(result == bincountSparse(x, weights))
+    }
   }
 }
 
