46a89817c4b01e61a2fd8d37f079c4b2bb4c78a0
Chris Stucchio <stucchio@gmail.com>
2016-02-28 15:07:48 -0500

    Chi2 hypothesis test

--- math/src/main/scala/breeze/stats/hypothesis/package.scala
+++ math/src/main/scala/breeze/stats/hypothesis/package.scala
@@ -37,4 +37,64 @@ package object hypothesis {
     val dof = n1-1
     new StudentsT(dof)(RandBasis.mt0).unnormalizedPdf(Z) //return p value
   }
+
+
+  case class Chi2Result(chi2: Double, pVal: Double)
+  private def chiSquaredTerm(e: Double, o: Double) = (e-o)*(e-o)/e
+
+  def chi2Test(successControl: Long, trialsControl: Long, successVariant: Long, trialsVariant: Long):Chi2Result = {
+    /*
+     * Takes 2 Bernoulli trials, and determines using a chi2 test whether there is a
+     * statistically significant difference between the probabilities of success
+     * between control and a variant.
+     */
+    val meanP = (successControl + successVariant).toDouble / (trialsControl + trialsVariant).toDouble
+    val chi2 = (chiSquaredTerm(meanP * trialsControl, successControl) + chiSquaredTerm((1-meanP) * trialsControl, trialsControl - successControl)
+              + chiSquaredTerm(meanP * trialsVariant, successVariant) + chiSquaredTerm((1-meanP) * trialsVariant, trialsVariant - successVariant))
+    val pVal = 1.0 - Gamma(0.5, 2.0).cdf(chi2)
+    Chi2Result(chi2, pVal)
+  }
+
+  def chi2Test(control: (Long, Long), trials: Seq[(Long, Long)]): Seq[Chi2Result] = {
+    /*
+     * Takes a sequence of N Bernoulli trials, and determines using a chi2 test whether there is a
+     * statistically significant difference between the N variants and a control.
+     * I.e., the variants may differ from each other, but this only determines whether
+     * they differ from control.
+     *
+     * The pVal reported in the results is the probability (assuming the null hypothesis) of
+     * a false positive at least this large in *any* variant, not in one particular variant.
+     * I.e., multiple comparisons are corrected for.
+     */
+    val numTrials = trials.size
+    trials.map( x => chi2Test(control._1, control._2, x._1, x._2) ).map(r => Chi2Result(r.chi2, sidakCorrectedPVal(r.pVal, numTrials)))
+  }
+
+  def sidakCorrectedPVal(p: Double, n: Int): Double = {
+    /*
+     * Takes a p-value run for a single statistical test,
+     * and then corrects for multiple comparisons.
+     *
+     * I.e., if you run n tests with a p-value cutoff of 5%
+     * yielding p-values p1, p2, ..., pn, then if
+     * sidakCorrectedPVal(p1,n) < 5% or sidakCorrectedPVal(p2, n) < 5%, etc,
+     * you can reject the null hypothesis.
+     */
+    1.0 - pow(1.0-p, n)
+  }
+
+  def sidakCorrectedPValCutoff(p: Double, n: Int): Double = {
+    /*
+     * Takes a p-value run for a single statistical test,
+     * and then corrects for multiple comparisons.
+     *
+     * This function is the inverse of sidakCorrectedPVal.
+     * If you run n tests and want a 5% chance of false positive (assuming null
+     * hypothesis is true) across *all* tests, then you can
+     * run each individual test with a p-value cutoff of
+     * sidakCorrectedPValCutoff(0.05, n).
+     */
+    1.0 - pow(1.0-p, 1.0/n.toDouble)
+  }
+
 }
