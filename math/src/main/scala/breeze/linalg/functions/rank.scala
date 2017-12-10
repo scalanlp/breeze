e3ba116422eb3147ad0fc3a9a684abf9e791e873
David Hall <dlwh@cs.berkeley.edu>
2017-12-10 14:08:18 -0800

    expose float rank

--- math/src/main/scala/breeze/linalg/functions/rank.scala
+++ math/src/main/scala/breeze/linalg/functions/rank.scala
@@ -2,9 +2,10 @@ package breeze.linalg
 
 import breeze.generic.UFunc
 import breeze.linalg.svd.SVD
-import com.github.fommil.netlib.LAPACK.{getInstance=>lapack}
+import com.github.fommil.netlib.LAPACK.{getInstance => lapack}
 import breeze.linalg.support.CanTraverseValues
 import breeze.linalg.support.CanTraverseValues.ValuesVisitor
+import breeze.math.Field
 
 /**
  * Computes the rank of a matrix.
@@ -19,20 +20,21 @@ import breeze.linalg.support.CanTraverseValues.ValuesVisitor
  * @return the rank of the matrix (number of singular values)
  */
 object rank extends UFunc {
-  implicit def implRankFromSVD[M, S](implicit canSVD: svd.Impl[M, SVD[_, S]],
-                                     maxS: max.Impl[S, Double],
-                                     travS: CanTraverseValues[S, Double]):Impl[M, Int] = {
+  implicit def implRankFromSVD[M, S, F](implicit canSVD: svd.Impl[M, SVD[_, S]],
+                                        maxS: max.Impl[S, F],
+                                        travS: CanTraverseValues[S, F],
+                                        nF: norm.Impl[F, Double]):Impl[M, Int] = {
     new Impl[M, Int] {
       def apply(m: M): Int = {
         val SVD(u, s, vt) = svd(m)
         // we called LAPACK for the SVD method, so this is the LAPACK definition of eps.
         val eps: Double = 2.0 * lapack.dlamch("e")
-        val tol = eps * max(s)
+        val tol = eps * norm(max(s))
         var n = 0
-        travS.traverse(s, new ValuesVisitor[Double] {
-          def visit(a: Double): Unit = if(a > tol) n += 1
+        travS.traverse(s, new ValuesVisitor[F] {
+          def visit(a: F): Unit = if(nF(a) > tol) n += 1
 
-          def zeros(numZero: Int, zeroValue: Double): Unit = ()
+          def zeros(numZero: Int, zeroValue: F): Unit = ()
         })
 
         n
@@ -42,12 +44,11 @@ object rank extends UFunc {
   }
 
   implicit def implRankTol[M, S](implicit canSVD: svd.Impl[M, (_, S, _)],
-                                     maxS: max.Impl[S, Double],
-                                     travS: CanTraverseValues[S, Double]):Impl2[M, Double, Int] = {
+                                 maxS: max.Impl[S, Double],
+                                 travS: CanTraverseValues[S, Double]):Impl2[M, Double, Int] = {
     new Impl2[M, Double, Int] {
       def apply(m: M, tol: Double): Int = {
         val (u, s, vt) = svd(m)
-        // we called LAPACK for the SVD method, so this is the LAPACK definition of eps.
         var n = 0
         travS.traverse(s, new ValuesVisitor[Double] {
           def visit(a: Double): Unit = if(a > tol) n += 1
