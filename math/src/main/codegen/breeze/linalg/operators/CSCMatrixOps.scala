c42dc257a9584ebb70b4460e8a846f0dab8282a7
Aaron Lee <aaron@test.com>
2021-05-01 21:16:36 +0100

    Convert to/from DenseMatrix for DenseVector*CSCMatrix

--- math/src/main/codegen/breeze/linalg/operators/CSCMatrixOps.scala
+++ math/src/main/codegen/breeze/linalg/operators/CSCMatrixOps.scala
@@ -57,16 +57,17 @@ trait CSCMatrixOps extends CSCMatrixOps_Ring { this: CSCMatrix.type =>
     }
 
   implicit def canMulDVt_CSC_eq_DVt[T](
-      implicit op: OpMulMatrix.Impl2[CSCMatrix[T], CSCMatrix[T], CSCMatrix[T]],
+      implicit op: OpMulMatrix.Impl2[DenseMatrix[T], CSCMatrix[T], DenseMatrix[T]],
       zero: Zero[T],
       ct: ClassTag[T]): OpMulMatrix.Impl2[Transpose[DenseVector[T]], CSCMatrix[T], Transpose[DenseVector[T]]] =
     new OpMulMatrix.Impl2[Transpose[DenseVector[T]], CSCMatrix[T], Transpose[DenseVector[T]]] {
       def apply(v: Transpose[DenseVector[T]], v2: CSCMatrix[T]): Transpose[DenseVector[T]] = {
         require(v2.rows == v.inner.length)
-        val csc = v.inner.asCscRow
-        val cscr = op(csc, v2)
 
-        new Transpose[DenseVector[T]](DenseVector.fromCSCMatrix(cscr))
+        val dm = v.inner.toDenseMatrix
+        val multiplied = op(dm, v2)
+
+        new Transpose[DenseVector[T]](multiplied.toDenseVector)
 
       }
     }
